79d9d6ed061ef3b1ed09b5854cdd822244a82828
acredsfan <acredsfan@gmail.com>
2023-05-06 12:12:56 -0400

    UI coding added

--- user_interface/web_interface/static/js/main.js
+++ user_interface/web_interface/static/js/main.js
@@ -1,1 +1,68 @@
-##Code goes here
+function move(direction) {
+    fetch('url_to_your_flask_route', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ direction: direction }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => console.error('Error:', error));
+}
+
+function startMowing() {
+    fetch('url_to_your_flask_route', {
+        method: 'POST',
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => console.error('Error:', error));
+}
+
+function stopMowing() {
+    fetch('url_to_your_flask_route', {
+        method: 'POST',
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => console.error('Error:', error));
+}
+
+function saveMowingArea(coordinates) {
+    fetch('url_to_your_flask_route', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ coordinates: coordinates }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => console.error('Error:', error));
+}
+
+
+document.getElementById('settings-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const mowDays = document.getElementById('mow-days').value;
+    const mowHours = document.getElementById('mow-hours').value;
+    // Get other settings inputs here
+
+    saveSettings(mowDays, mowHours);
+});
+
+function saveSettings(mowDays, mowHours) {
+    fetch('url_to_your_flask_route', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ mowDays: mowDays, mowHours: mowHours }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => console.error('Error:', error));
+}
+
