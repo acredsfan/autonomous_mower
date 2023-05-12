fd93776f666070f36aabf2cc5a257ae4702f8ef0
acredsfan <acredsfan@gmail.com>
2023-05-12 12:25:37 -0400

    Update test_mpu9250.py

--- tests/test_mpu9250.py
+++ tests/test_mpu9250.py
@@ -1,40 +1,20 @@
-import os
-import sys
-import time
-import smbus
+# MPU6050 9-DoF Example Printout
 
-from imusensor.MPU9250 import MPU9250
+from hardware_interface.mpu9250_i2c import *
 
-address = 0x68
-bus = smbus.SMBus(1)
-imu = MPU9250.MPU9250(bus, address)
+time.sleep(1) # delay necessary to allow mpu9250 to settle
 
-def readSensor():
-	try:
-		imu.readSensor()
-	except Exception as e:
-		print("Error reading sensor:", e)
-		return None
-	return imu.AccelVals, imu.GyroVals, imu.MagVals
-
-def computeOrientation():
-	try:
-		imu.computeOrientation()
-	except Exception as e:
-		print("Error computing orientation:", e)
-		return None
-	return imu.roll, imu.pitch, imu.yaw
-
-def main():
-	while True:
-		accel, gyro, mag = readSensor()
-		roll, pitch, yaw = computeOrientation()
-
-		print ("Accel x: {0} ; Accel y : {1} ; Accel z : {2}".format(accel[0], accel[1], accel[2]))
-		print ("Gyro x: {0} ; Gyro y : {1} ; Gyro z : {2}".format(gyro[0], gyro[1], gyro[2]))
-		print ("Mag x: {0} ; Mag y : {1} ; Mag z : {2}".format(mag[0], mag[1], mag[2]))
-		print ("roll: {0} ; pitch : {1} ; yaw : {2}".format(roll, pitch, yaw))
-		time.sleep(0.1)
-
-if __name__ == "__main__":
-	main()
+print('recording data')
+while 1:
+    try:
+        ax,ay,az,wx,wy,wz = mpu6050_conv() # read and convert mpu6050 data
+        mx,my,mz = AK8963_conv() # read and convert AK8963 magnetometer data
+    except:
+        continue
+    
+    print('{}'.format('-'*30))
+    print('accel [g]: x = {0:2.2f}, y = {1:2.2f}, z {2:2.2f}= '.format(ax,ay,az))
+    print('gyro [dps]:  x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(wx,wy,wz))
+    print('mag [uT]:   x = {0:2.2f}, y = {1:2.2f}, z = {2:2.2f}'.format(mx,my,mz))
+    print('{}'.format('-'*30))
+    time.sleep(1)
