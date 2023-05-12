de670f800d02fb873a2fd5eb870978b340c43063
acredsfan <acredsfan@gmail.com>
2023-05-12 12:13:22 -0400

    Update test_mpu9250.py

--- tests/test_mpu9250.py
+++ tests/test_mpu9250.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 import time
@@ -6,17 +5,36 @@ import smbus
 
 from imusensor.MPU9250 import MPU9250
 
-address = 0x68
-bus = smbus.SMBus(0)
+address = 0x69
+bus = smbus.SMBus(1)
 imu = MPU9250.MPU9250(bus, address)
-imu.begin()
 
-while True:
-	imu.readSensor()
-	imu.computeOrientation()
+def readSensor():
+	try:
+		imu.readSensor()
+	except Exception as e:
+		print("Error reading sensor:", e)
+		return None
+	return imu.AccelVals, imu.GyroVals, imu.MagVals
 
-	print ("Accel x: {0} ; Accel y : {1} ; Accel z : {2}".format(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2]))
-	print ("Gyro x: {0} ; Gyro y : {1} ; Gyro z : {2}".format(imu.GyroVals[0], imu.GyroVals[1], imu.GyroVals[2]))
-	print ("Mag x: {0} ; Mag y : {1} ; Mag z : {2}".format(imu.MagVals[0], imu.MagVals[1], imu.MagVals[2]))
-	print ("roll: {0} ; pitch : {1} ; yaw : {2}".format(imu.roll, imu.pitch, imu.yaw))
-	time.sleep(0.1)
+def computeOrientation():
+	try:
+		imu.computeOrientation()
+	except Exception as e:
+		print("Error computing orientation:", e)
+		return None
+	return imu.roll, imu.pitch, imu.yaw
+
+def main():
+	while True:
+		accel, gyro, mag = readSensor()
+		roll, pitch, yaw = computeOrientation()
+
+		print ("Accel x: {0} ; Accel y : {1} ; Accel z : {2}".format(accel[0], accel[1], accel[2]))
+		print ("Gyro x: {0} ; Gyro y : {1} ; Gyro z : {2}".format(gyro[0], gyro[1], gyro[2]))
+		print ("Mag x: {0} ; Mag y : {1} ; Mag z : {2}".format(mag[0], mag[1], mag[2]))
+		print ("roll: {0} ; pitch : {1} ; yaw : {2}".format(roll, pitch, yaw))
+		time.sleep(0.1)
+
+if __name__ == "__main__":
+	main()
