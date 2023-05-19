b9f23db0c07ccae407966caebb44c97d9b0744c3
acredsfan <acredsfan@gmail.com>
2023-05-19 15:08:54 -0400

    Update __init__.py

--- hardware_interface/__init__.py
+++ hardware_interface/__init__.py
@@ -4,11 +4,9 @@
 from .motor_controller import MotorController
 from .relay_controller import RelayController
 from .sensor_interface import SensorInterface
-from .mpu9250_i2c import mpu9250_i2c
 
 __all__ = [
     'MotorController',
     'RelayController',
-    'SensorInterface',
-    'mpu9250_i2c'
+    'SensorInterface'
 ]
