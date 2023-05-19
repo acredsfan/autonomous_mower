e1f8b58ac4df8c96b061a8474f69e17c215f54dd
acredsfan <acredsfan@gmail.com>
2023-05-19 13:51:19 -0400

    Update test_tof2.py

--- test_tof2.py
+++ test_tof2.py
@@ -1,69 +1,38 @@
 import time
 import VL53L0X
-import RPi.GPIO as GPIO
 
-# GPIO for Sensor 1 shutdown pin
-sensor1_shutdown = 22
-# GPIO for Sensor 2 shutdown pin
-sensor2_shutdown = 23
+# Create a VL53L0X object for device on TCA9548A bus 4
+vl53l0x_right = VL53L0X.VL53L0X(tca9548a_num=4, tca9548a_addr=0x70)
+# Create a VL53L0X object for device on TCA9548A bus 5
+vl53l0x_left = VL53L0X.VL53L0X(tca9548a_num=5, tca9548a_addr=0x70)
+vl53l0x_right.open()
+vl53l0x_left.open()
 
-GPIO.setwarnings(False)
+# Start ranging on TCA9548A bus 1
+vl53l0x_right.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+# Start ranging on TCA9548A bus 2
+vl53l0x_left.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
-# Setup GPIO for shutdown pins on each VL53L0X
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(sensor1_shutdown, GPIO.OUT)
-GPIO.setup(sensor2_shutdown, GPIO.OUT)
-
-# Set all shutdown pins low to turn off each VL53L0X
-GPIO.output(sensor1_shutdown, GPIO.LOW)
-GPIO.output(sensor2_shutdown, GPIO.LOW)
-
-# Keep all low for 500 ms or so to make sure they reset
-time.sleep(0.50)
-
-# Create one object per VL53L0X passing the address to give to
-# each.
-tof = VL53L0X.VL53L0X(i2c_address=0x29)
-tof1 = VL53L0X.VL53L0X(i2c_address=0x2A)
-tof.open()
-tof1.open()
-
-# Set shutdown pin high for the first VL53L0X then 
-# call to start ranging 
-GPIO.output(sensor1_shutdown, GPIO.HIGH)
-time.sleep(0.50)
-tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
-
-# Set shutdown pin high for the second VL53L0X then 
-# call to start ranging 
-GPIO.output(sensor2_shutdown, GPIO.HIGH)
-time.sleep(0.50)
-tof1.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
-
-timing = tof.get_timing()
+timing = vl53l0x_right.get_timing()
 if timing < 20000:
     timing = 20000
 print("Timing %d ms" % (timing/1000))
 
-for count in range(1,101):
-    distance = tof.get_distance()
+for count in range(1, 101):
+    # Get distance from VL53L0X  on TCA9548A bus 1
+    distance = vl53l0x_right.get_distance()
     if distance > 0:
-        print("sensor %d - %d mm, %d cm, iteration %d" % (1, distance, (distance/10), count))
-    else:
-        print("%d - Error" % 1)
+        print("1: %d mm, %d cm, %d" % (distance, (distance/10), count))
 
-    distance = tof1.get_distance()
+    # Get distance from VL53L0X  on TCA9548A bus 2
+    distance = vl53l0x_left.get_distance()
     if distance > 0:
-        print("sensor %d - %d mm, %d cm, iteration %d" % (2, distance, (distance/10), count))
-    else:
-        print("%d - Error" % 2)
+        print("2: %d mm, %d cm, %d" % (distance, (distance/10), count))
 
     time.sleep(timing/1000000.00)
 
-tof1.stop_ranging()
-GPIO.output(sensor2_shutdown, GPIO.LOW)
-tof.stop_ranging()
-GPIO.output(sensor1_shutdown, GPIO.LOW)
+vl53l0x_right.stop_ranging()
+vl53l0x_left.stop_ranging()
 
-tof.close()
-tof1.close()
+vl53l0x_right.close()
+vl53l0x_left.close()
