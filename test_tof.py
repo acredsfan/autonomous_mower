268209e703f6d604e4db6a7d16147a5e40c2f54e
acredsfan <acredsfan@gmail.com>
2023-05-19 13:41:43 -0400

    Update test_tof.py

--- test_tof.py
+++ test_tof.py
@@ -5,52 +5,52 @@ import adafruit_vl53l0x
 import RPi.GPIO as GPIO
 
 # Set up I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
+i2c_bus = busio.I2C(board.SCL, board.SDA)
 
-# Set the GPIO pin numbers connected to the XSHUT pins of the left and right sensors
-left_xshut_pin = 22  # GPIO pin connected to the left sensor's XSHUT pin
-right_xshut_pin = 23  # GPIO pin connected to the right sensor's XSHUT pin
+# Set the GPIO pin numbers connected to the multiplexer channel select pins
+mux_address_pins = [4, 5, 6]  # GPIO pins connected to A0, A1, A2 address pins of the multiplexer
 
 # Set up the GPIO pins
 if not GPIO.getmode():
     GPIO.setmode(GPIO.BCM)
-GPIO.setup(left_xshut_pin, GPIO.OUT)
-GPIO.setup(right_xshut_pin, GPIO.OUT)
+GPIO.setup(mux_address_pins, GPIO.OUT)
 
-def initialize_sensor(xshut_pin, i2c, address):
-    print(f"Resetting and initializing sensor with address {hex(address)}...")
+# Function to select the desired channel on the I2C multiplexer
+def select_mux_channel(channel):
+    # Convert the channel number to binary and set the address pins accordingly
+    binary_channel = bin(channel)[2:].zfill(len(mux_address_pins))
+    GPIO.output(mux_address_pins, list(map(int, binary_channel)))
+
+# Reset the multiplexer to select the default channel (0)
+select_mux_channel(0)
+
+# Create a function to initialize the VL53L0X sensor
+def initialize_sensor(i2c, address):
     # Reset and initialize the sensor
-    GPIO.output(xshut_pin, GPIO.LOW)
-    time.sleep(0.1)
-    GPIO.output(xshut_pin, GPIO.HIGH)
-    time.sleep(0.1)
     sensor = adafruit_vl53l0x.VL53L0X(i2c=i2c)
     sensor.set_address(address)
-    print(f"Sensor with address {hex(address)} initialized successfully.")
     return sensor
 
 # Initialize the right sensor (address 0x29)
-tof_right = initialize_sensor(right_xshut_pin, i2c, 0x30)
+select_mux_channel(0)  # Select the multiplexer channel for the right sensor
+tof_right = initialize_sensor(i2c_bus, 0x29)
 
 # Initialize the left sensor (address 0x2A)
-tof_left = initialize_sensor(left_xshut_pin, i2c, 0x31)
+select_mux_channel(1)  # Select the multiplexer channel for the left sensor
+tof_left = initialize_sensor(i2c_bus, 0x2A)
 
 # Start continuous mode for both sensors
-print("Starting continuous mode for both sensors...")
 tof_right.start_continuous()
 tof_left.start_continuous()
 
 # Read distance from right sensor
-print("Reading distance from right sensor...")
 distance_right = tof_right.range
 print("Distance right:", distance_right)
 
 # Read distance from left sensor
-print("Reading distance from left sensor...")
 distance_left = tof_left.range
 print("Distance left:", distance_left)
 
 # Stop the continuous mode for both sensors
-print("Stopping continuous mode for both sensors...")
 tof_right.stop_continuous()
 tof_left.stop_continuous()
