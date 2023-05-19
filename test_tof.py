9e1ca9fe26a0ea5f226b52d1ae9642e73c99c3d5
acredsfan <acredsfan@gmail.com>
2023-05-19 09:44:57 -0400

    Update test_tof.py

--- test_tof.py
+++ test_tof.py
@@ -5,9 +5,9 @@ import busio
 import board
 
 print("Imports successful, initializing VL53L0X sensors")
-vl53l0x_left = adafruit_vl53l0x.VL53L0X(i2c=busio.I2C(board.SCL, board.SDA), address=0x29)
+vl53l0x_left = adafruit_vl53l0x.VL53L0X()
 print("Left sensor initialized successfully with address 0x29")
-vl53l0x_right = adafruit_vl53l0x.VL53L0X(i2c=busio.I2C(board.SCL, board.SDA), address=0x2a)
+#vl53l0x_right = adafruit_vl53l0x.VL53L0X(i2c=busio.I2C(board.SCL, board.SDA), address=0x2a)
 print("Right sensor initialized successfully with address 0x2a")
 
 print("Starting test")
@@ -26,15 +26,15 @@ try:
         print("tof_left: ", readRangeResult()) # Print the range result for the left sensor
 
         # Reading from the right sensor
-        startRange() # Start a range operation for the right sensor
-        while not isRangeComplete(): # Wait for the range operation to complete
-            time.sleep(0.01) # Sleep for 10ms
+       # startRange() # Start a range operation for the right sensor
+       # while not isRangeComplete(): # Wait for the range operation to complete
+       #     time.sleep(0.01) # Sleep for 10ms
 
-        if readRangeStatus() != 0: # Check if there was an error in the range operation
-            print("Error reading from the right sensor")
-            break
+       # if readRangeStatus() != 0: # Check if there was an error in the range operation
+        #    print("Error reading from the right sensor")
+        #    break
 
-        print("tof_right: ", readRangeResult()) # Print the range result for the right sensor
+       # print("tof_right: ", readRangeResult()) # Print the range result for the right sensor
 
         time.sleep(0.1)
 
