f7e8ba93d7ffde68ec2733e9e284983fc366f06e
acredsfan <acredsfan@gmail.com>
2023-05-19 14:25:44 -0400

    Update sensor_interface.py

--- hardware_interface/sensor_interface.py
+++ hardware_interface/sensor_interface.py
@@ -15,14 +15,16 @@ import busio
 #from gps_interface import GPSInterface
 
 # Global variables
+MUX_ADDRESS = 0x70  # Replace with your multiplexer's I2C address if different
 bus = smbus.SMBus(1)
 #gps_serial = None
 i2c = board.I2C()   # uses board.SCL and board.SDA
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-vl53l0x_left = adafruit_vl53l0x.VL53L0X(i2c=busio.I2C(board.SCL, board.SDA), address=0x29)
-vl53l0x_right = adafruit_vl53l0x.VL53L0X(i2c=busio.I2C(board.SCL, board.SDA), address=0x2a)
-mpu9250 = adafruit_mpu9250.MPU9250()
-ina3221 = INA3221(bus)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(tca9548a_num=2, tca9548a_addr=0x70)
+vl53l0x_right = VL53L0X.VL53L0X(tca9548a_num=4, tca9548a_addr=0x70)
+vl53l0x_left = VL53L0X.VL53L0X(tca9548a_num=5, tca9548a_addr=0x70)
+mpu9250_master = adafruit_mpu9250.MPU9250(tca9548a_num=0, tca9548a_addr=0x70)
+mpu9250_slave = adafruit_mpu9250.MPU9250(tca9548a_num=1, tca9548a_addr=0x70)
+ina3221 = INA3221(tca9548a_num=3, tca9548a_addr=0x70)
 HALL_EFFECT_SENSOR_1 = 17  # Replace with the correct GPIO pin number for sensor 1
 HALL_EFFECT_SENSOR_2 = 18  # Replace with the correct GPIO pin number for sensor 2
 # change this to match the location's pressure (hPa) at sea level
@@ -31,6 +33,13 @@ bme280.sea_level_pressure = 1013.25
 class SensorInterface:
 
 #FUNCTIONS
+    def select_mux_channel(channel):
+        """Select the specified channel on the TCA9548A I2C multiplexer."""
+        if 0 <= channel <= 7:
+            bus.write_byte(MUX_ADDRESS, 1 << channel)
+        else:
+            raise ValueError("Multiplexer channel must be an integer between 0 and 7.")
+    
     def init_hall_effect_sensors():
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(HALL_EFFECT_SENSOR_1, GPIO.IN)
@@ -48,8 +57,15 @@ class SensorInterface:
         bme280.begin()
 
         # Initialize VL53L0X sensors
-        vl53l0x_left.begin()
-        vl53l0x_right.begin()
+        # Start ranging on TCA9548A bus 1
+        vl53l0x_right.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+        # Start ranging on TCA9548A bus 2
+        vl53l0x_left.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+
+        timing = vl53l0x_right.get_timing()
+        if timing < 20000:
+            timing = 20000
+        print("Timing %d ms" % (timing/1000))
 
         # Initialize MPU9250
         mpu9250.begin()
@@ -78,11 +94,11 @@ class SensorInterface:
 
     def read_vl53l0x_left():
         """Read VL53L0X left sensor data."""
-        return vl53l0x_left.range
+        return vl53l0x_left.get_distance()
 
     def read_vl53l0x_right():
         """Read VL53L0X right sensor data."""
-        return vl53l0x_right.range
+        return vl53l0x_right.get_distance()
 
     def read_mpu9250_compass():
         """Read MPU9250 compass data."""
