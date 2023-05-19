ec4958f2d1078fbe51f2f907289ed08e9321d170
acredsfan <acredsfan@gmail.com>
2023-05-19 14:53:19 -0400

    Update sensor_interface.py

--- hardware_interface/sensor_interface.py
+++ hardware_interface/sensor_interface.py
@@ -6,7 +6,7 @@ import smbus
 import time
 import board
 from adafruit_bme280 import basic as adafruit_bme280
-import adafruit_vl53l0x
+import VL53L0X
 import FaBo9Axis_MPU9250
 from barbudor_ina3221.lite import INA3221
 import RPi.GPIO as GPIO
@@ -14,77 +14,77 @@ import serial
 import busio
 #from gps_interface import GPSInterface
 
-# Global variables
-MUX_ADDRESS = 0x70  # Replace with your multiplexer's I2C address if different
-bus = smbus.SMBus(1)
-#gps_serial = None
-i2c = board.I2C()   # uses board.SCL and board.SDA
-bme280 = adafruit_bme280.Adafruit_BME280_I2C(tca9548a_num=2, tca9548a_addr=0x70)
-vl53l0x_right = VL53L0X.VL53L0X(tca9548a_num=4, tca9548a_addr=0x70)
-vl53l0x_left = VL53L0X.VL53L0X(tca9548a_num=5, tca9548a_addr=0x70)
-mpu9250_master = adafruit_mpu9250.MPU9250(tca9548a_num=0, tca9548a_addr=0x70)
-mpu9250_slave = adafruit_mpu9250.MPU9250(tca9548a_num=1, tca9548a_addr=0x70)
-ina3221 = INA3221(tca9548a_num=3, tca9548a_addr=0x70)
-HALL_EFFECT_SENSOR_1 = 17  # Replace with the correct GPIO pin number for sensor 1
-HALL_EFFECT_SENSOR_2 = 18  # Replace with the correct GPIO pin number for sensor 2
-# change this to match the location's pressure (hPa) at sea level
-bme280.sea_level_pressure = 1013.25
-
 class SensorInterface:
+    def __init__(self):
+        self.MUX_ADDRESS = 0x70  # Replace with your multiplexer's I2C address if different
+        self.bus = smbus.SMBus(1)
+        # self.gps_serial = None
+        self.i2c = board.I2C()  # uses board.SCL and board.SDA
+        self.bme280 = adafruit_bme280.Adafruit_BME280_I2C(tca9548a_num=2, tca9548a_addr=0x70)
+        self.vl53l0x_right = adafruit_vl53l0x.VL53L0X(tca9548a_num=4, tca9548a_addr=0x70)
+        self.vl53l0x_left = adafruit_vl53l0x.VL53L0X(tca9548a_num=5, tca9548a_addr=0x70)
+        self.mpu9250_master = FaBo9Axis_MPU9250.MPU9250(tca9548a_num=0, tca9548a_addr=0x70)
+        self.mpu9250_slave = FaBo9Axis_MPU9250.MPU9250(tca9548a_num=1, tca9548a_addr=0x70)
+        self.ina3221 = INA3221(tca9548a_num=3, tca9548a_addr=0x70)
+        self.HALL_EFFECT_SENSOR_1 = 17  # Replace with the correct GPIO pin number for sensor 1
+        self.HALL_EFFECT_SENSOR_2 = 18  # Replace with the correct GPIO pin number for sensor 2
+        # change this to match the location's pressure (hPa) at sea level
+        self.bme280.sea_level_pressure = 1013.25
 
-#FUNCTIONS
-    def select_mux_channel(channel):
+    # FUNCTIONS
+    def select_mux_channel(self, channel):
         """Select the specified channel on the TCA9548A I2C multiplexer."""
         if 0 <= channel <= 7:
-            bus.write_byte(MUX_ADDRESS, 1 << channel)
+            self.bus.write_byte(self.MUX_ADDRESS, 1 << channel)
         else:
             raise ValueError("Multiplexer channel must be an integer between 0 and 7.")
-    
-    def init_hall_effect_sensors():
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(HALL_EFFECT_SENSOR_1, GPIO.IN)
-        GPIO.setup(HALL_EFFECT_SENSOR_2, GPIO.IN)
 
-    def init_sensors():
+    def init_hall_effect_sensors(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.HALL_EFFECT_SENSOR_1, GPIO.IN)
+        GPIO.setup(self.HALL_EFFECT_SENSOR_2, GPIO.IN)
+
+    def init_sensors(self):
         """Initialize all sensors."""
-        #global gps_serial  # Use the global GPS serial object
+        # global gps_serial  # Use the global GPS serial object
 
         # Initialize GPS
-        #gps_serial = GPSInterface()  # Adjust the port and baud rate if needed
-        #gps_serial.flush()
+        # gps_serial = GPSInterface()  # Adjust the port and baud rate if needed
+        # gps_serial.flush()
 
         # Initialize BME280
-        bme280.begin()
+        self.bme280.begin()
 
         # Initialize VL53L0X sensors
         # Start ranging on TCA9548A bus 1
-        vl53l0x_right.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+        self.vl53l0x_right.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
         # Start ranging on TCA9548A bus 2
-        vl53l0x_left.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+        self.vl53l0x_left.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
-        timing = vl53l0x_right.get_timing()
+        timing = self.vl53l0x_right.get_timing()
         if timing < 20000:
             timing = 20000
-        print("Timing %d ms" % (timing/1000))
+        print("Timing %d ms" % (timing / 1000))
 
         # Initialize MPU9250
-        mpu9250.begin()
+        self.mpu9250_master.begin()
+        self.mpu9250_slave.begin()
 
         # Initialize INA3221
-        ina3221.begin()
+        self.ina3221.begin()
 
         # Initialize hall effect sensors
-        init_hall_effect_sensors()
+        self.init_hall_effect_sensors()
 
-    def read_bme280():
+    def read_bme280(self):
         """Read BME280 sensor data."""
         return {
-            'temperature': bme280.temperature,
-            'humidity': bme280.relative_humidity,
-            'pressure': bme280.pressure
+            'temperature': self.bme280.temperature,
+            'humidity': self.bme280.relative_humidity,
+            'pressure': self.bme280.pressure
         }
 
-    #def read_gps():
+    # def read_gps():
     #    """Read GPS data."""
     #    if gps_serial is None:
     #        raise Exception("GPS module not initialized")
@@ -92,35 +92,35 @@ class SensorInterface:
     #    gps_data = gps_serial.read_gps_data()
     #    return gps_data
 
-    def read_vl53l0x_left():
+    def read_vl53l0x_left(self):
         """Read VL53L0X left sensor data."""
-        return vl53l0x_left.get_distance()
+        return self.vl53l0x_left.get_distance()
 
-    def read_vl53l0x_right():
+    def read_vl53l0x_right(self):
         """Read VL53L0X right sensor data."""
-        return vl53l0x_right.get_distance()
+        return self.vl53l0x_right.get_distance()
 
-    def read_mpu9250_compass():
+    def read_mpu9250_compass(self):
         """Read MPU9250 compass data."""
-        return mpu9250.read_compass()
+        return self.mpu9250_master.read_compass()
 
-    def read_mpu9250_gyro():
+    def read_mpu9250_gyro(self):
         """Read MPU9250 gyro data."""
-        return mpu9250.read_gyro()
+        return self.mpu9250_master.read_gyro()
 
-    def read_ina3221():
+    def read_ina3221(self):
         """Read INA3221 power monitor data."""
-        return ina3221.read_all()
+        return self.ina3221.read_all()
 
-    def read_hall_effect_sensors():
-        sensor_1_state = GPIO.input(HALL_EFFECT_SENSOR_1)
-        sensor_2_state = GPIO.input(HALL_EFFECT_SENSOR_2)
+    def read_hall_effect_sensors(self):
+        sensor_1_state = GPIO.input(self.HALL_EFFECT_SENSOR_1)
+        sensor_2_state = GPIO.input(self.HALL_EFFECT_SENSOR_2)
 
         return sensor_1_state, sensor_2_state
 
-    def ideal_mowing_conditions():
+    def ideal_mowing_conditions(self):
         # Check for high humidity
-        bme280_data = SensorInterface.read_bme280()
+        bme280_data = self.read_bme280()
         if bme280_data['humidity'] > 90:
             return False
 
