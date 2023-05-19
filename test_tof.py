2595c5b44e188ea6c998f8c266c4d7264f09ea9a
acredsfan <acredsfan@gmail.com>
2023-05-19 11:40:29 -0400

    Update test_tof.py

--- test_tof.py
+++ test_tof.py
@@ -18,6 +18,7 @@ GPIO.setup(left_xshut_pin, GPIO.OUT)
 GPIO.setup(right_xshut_pin, GPIO.OUT)
 
 def initialize_sensor(xshut_pin, i2c, address):
+    print(f"Resetting and initializing sensor with address {hex(address)}...")
     # Reset and initialize the sensor
     GPIO.output(xshut_pin, GPIO.LOW)
     time.sleep(0.1)
@@ -25,6 +26,7 @@ def initialize_sensor(xshut_pin, i2c, address):
     time.sleep(0.1)
     sensor = adafruit_vl53l0x.VL53L0X(i2c=i2c)
     sensor.set_address(address)
+    print(f"Sensor with address {hex(address)} initialized successfully.")
     return sensor
 
 # Initialize the right sensor (address 0x29)
@@ -34,17 +36,21 @@ tof_right = initialize_sensor(right_xshut_pin, i2c, 0x29)
 tof_left = initialize_sensor(left_xshut_pin, i2c, 0x2A)
 
 # Start continuous mode for both sensors
+print("Starting continuous mode for both sensors...")
 tof_right.start_continuous()
 tof_left.start_continuous()
 
 # Read distance from right sensor
+print("Reading distance from right sensor...")
 distance_right = tof_right.range
 print("Distance right:", distance_right)
 
 # Read distance from left sensor
+print("Reading distance from left sensor...")
 distance_left = tof_left.range
 print("Distance left:", distance_left)
 
 # Stop the continuous mode for both sensors
+print("Stopping continuous mode for both sensors...")
 tof_right.stop_continuous()
 tof_left.stop_continuous()
