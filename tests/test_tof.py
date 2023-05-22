29e31814802fd20212a7426ddb9fc92bd0528894
acredsfan <acredsfan@gmail.com>
2023-05-22 14:18:03 -0400

    Update test_tof.py

--- tests/test_tof.py
+++ tests/test_tof.py
@@ -4,8 +4,8 @@ from hardware_interface import sensor_interface
 
 try:
     while True:
-        print("tof_left: ", sensor_interface.read_VL53L0X_left())
-        print("tof_right: ", sensor_interface.read_VL53L0X_right())
+        print("tof_left: ", sensor_interface.read_vl53l0x_left())
+        print("tof_right: ", sensor_interface.read_VL53l0X_right())
 
         time.sleep(0.1)
 
