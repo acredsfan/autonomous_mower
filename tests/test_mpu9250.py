f00185a3624c19d406cba1904e35e6547a291190
acredsfan <acredsfan@gmail.com>
2023-05-22 12:36:54 -0400

    Update test_mpu9250.py

--- tests/test_mpu9250.py
+++ tests/test_mpu9250.py
@@ -1,28 +1,33 @@
 # MPU6050 9-DoF Example Printout
-
 import time
 from mpu9250_jmdev.registers import *
 from mpu9250_jmdev.mpu_9250 import MPU9250
 
-# Initialize the MPU9250 module.
 mpu = MPU9250(
     address_ak=AK8963_ADDRESS, 
-    address_mpu_master=MPU9050_ADDRESS_69,  # In 0x69 Address
-    address_mpu_slave=MPU9050_ADDRESS_69,   
-    bus=1,
+    address_mpu_master=MPU9050_ADDRESS_68, # Master has 0x68 Address
+    address_mpu_slave=MPU9050_ADDRESS_68, # Slave has 0x68 Address
+    bus=1, 
     gfs=GFS_1000, 
     afs=AFS_8G, 
     mfs=AK8963_BIT_16, 
     mode=AK8963_MODE_C100HZ)
 
-# Apply the settings to the registers.
-mpu.configure()
-#mpu.calibrate()
+mpu.configure() # Apply the settings to the registers.
 
-# Loop to print sensor data.
 while True:
+   
+    print("|.....MPU9250 in 0x68 I2C Bus - Master.....|")
     print("Accelerometer", mpu.readAccelerometerMaster())
     print("Gyroscope", mpu.readGyroscopeMaster())
     print("Magnetometer", mpu.readMagnetometerMaster())
-    print("Temperature", (mpu.readTemperatureMaster()*1.8)+32)
+    print("Temperature", mpu.readTemperatureMaster())
+    print("\n")
+
+    print("|.....MPU9250 in 0x68 I2C Bus - Slave in 0x68 auxiliary sensor address.....|")
+    print("Accelerometer", mpu.readAccelerometerSlave())
+    print("Gyroscope", mpu.readGyroscopeSlave())
+    print("Temperature", mpu.readTemperatureSlave())
+    print("\n")
+
     time.sleep(1)
