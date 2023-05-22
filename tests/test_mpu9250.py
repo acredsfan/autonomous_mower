05425182da9939ec928e05d5478a531bdb8fe864
acredsfan <acredsfan@gmail.com>
2023-05-22 12:37:40 -0400

    Update test_mpu9250.py

--- tests/test_mpu9250.py
+++ tests/test_mpu9250.py
@@ -5,8 +5,8 @@ from mpu9250_jmdev.mpu_9250 import MPU9250
 
 mpu = MPU9250(
     address_ak=AK8963_ADDRESS, 
-    address_mpu_master=MPU9050_ADDRESS_68, # Master has 0x68 Address
-    address_mpu_slave=MPU9050_ADDRESS_68, # Slave has 0x68 Address
+    address_mpu_master=MPU9050_ADDRESS_69, # Master has 0x68 Address
+    address_mpu_slave=MPU9050_ADDRESS_69, # Slave has 0x68 Address
     bus=1, 
     gfs=GFS_1000, 
     afs=AFS_8G, 
