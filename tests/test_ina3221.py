4e53a52d98cd75c68a9f2625f08183f3ae8bdf8f
acredsfan <acredsfan@gmail.com>
2023-05-22 14:58:37 -0400

    Create test_ina3221.py

---
+++ tests/test_ina3221.py
@@ -0,0 +1,34 @@
+from smbus2 import SMBus, i2c_msg
+import ina3221
+import time
+import busio
+import board
+
+# Define the I2C bus
+bus = SMBus(1)
+
+# Define the I2C address of the TCA9548A I2C multiplexer
+TCA9548A_I2C_ADDR = 0x70
+
+# Function to switch the I2C channel
+def tca_select(channel):
+    if channel > 7:
+        return
+    bus.write_byte(TCA9548A_I2C_ADDR, 1<<channel)
+
+# Select the I2C channel
+tca_select(2)
+
+# Create an I2C object
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create an INA3221 object
+ina = ina3221.INA3221(i2c)
+
+# Infinite loop
+while True:
+    # Print the voltage from each channel
+    print(ina.voltage(1), ina.voltage(2), ina.voltage(3))
+
+    # Sleep for 0.1 seconds
+    time.sleep(0.1)
