a0bb7a81289dab5d588edb9aa7e1942763efff95
acredsfan <acredsfan@gmail.com>
2023-05-22 12:50:46 -0400

    Update test_mpu9250.py

--- tests/test_mpu9250.py
+++ tests/test_mpu9250.py
@@ -2,18 +2,17 @@ import time
 from mpu9250_jmdev.registers import *
 from mpu9250_jmdev.mpu_9250 import MPU9250
 
-# Initialize the MPU9250 sensor
 mpu = MPU9250(
     address_ak=AK8963_ADDRESS, 
-    address_mpu_master=0x69 # In 0x69 Address
-    bus=1,  # SDA and SCL are connected to GPIO2 and GPIO3 respectively, which correspond to I2C bus 1 on Raspberry Pi
+    address_mpu_master=0x69,  # In 0x69 Address
+    bus=1,  
     gfs=GFS_1000, 
     afs=AFS_8G, 
     mfs=AK8963_BIT_16, 
     mode=AK8963_MODE_C100HZ
 )
 
-mpu.configure()  # Apply the settings to the registers
+mpu.configure()
 
 print("MPU9250 id: " + hex(mpu.whoami))
 
@@ -23,4 +22,4 @@ while True:
     print("Magnetometer", mpu.readMagnetometerMaster())
     print("Temperature", mpu.readTemperatureMaster())
 
-    time.sleep(1)  # Sleep for 1 second
+    time.sleep(1)
