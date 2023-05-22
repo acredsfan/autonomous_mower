f9496081e241aba838607b251d0a5a2eca9f180a
acredsfan <acredsfan@gmail.com>
2023-05-22 14:21:57 -0400

    Update test_tof.py

--- tests/test_tof.py
+++ tests/test_tof.py
@@ -1,11 +1,15 @@
 import sys
+import time
 sys.path.append('/home/pi/autonomous_mower')
-from hardware_interface import sensor_interface
+from hardware_interface.sensor_interface import SensorInterface
+
+sensor_interface_instance = SensorInterface()
+sensor_interface_instance.init_sensors()
 
 try:
     while True:
-        print("tof_left: ", sensor_interface.read_vl53l0x_left())
-        print("tof_right: ", sensor_interface.read_vl53l0x_right())
+        print("tof_left: ", sensor_interface_instance.read_vl53l0x_left())
+        print("tof_right: ", sensor_interface_instance.read_vl53l0x_right())
 
         time.sleep(0.1)
 
