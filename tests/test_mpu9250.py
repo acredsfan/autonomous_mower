6b22ebb7326fbf142e70e5cc0ecb551f7a87440a
acredsfan <acredsfan@gmail.com>
2023-05-22 12:47:14 -0400

    Update test_mpu9250.py

--- tests/test_mpu9250.py
+++ tests/test_mpu9250.py
@@ -1,33 +1,26 @@
-# MPU6050 9-DoF Example Printout
 import time
 from mpu9250_jmdev.registers import *
 from mpu9250_jmdev.mpu_9250 import MPU9250
 
+# Initialize the MPU9250 sensor
 mpu = MPU9250(
     address_ak=AK8963_ADDRESS, 
-    address_mpu_master=MPU9050_ADDRESS_69, # Master has 0x68 Address
-    address_mpu_slave=MPU9050_ADDRESS_69, # Slave has 0x68 Address
-    bus=1, 
+    address_mpu_master=MPU9250_ADDRESS_69, # In 0x68 Address
+    bus=1,  # SDA and SCL are connected to GPIO2 and GPIO3 respectively, which correspond to I2C bus 1 on Raspberry Pi
     gfs=GFS_1000, 
     afs=AFS_8G, 
     mfs=AK8963_BIT_16, 
-    mode=AK8963_MODE_C100HZ)
+    mode=AK8963_MODE_C100HZ
+)
 
-mpu.configure() # Apply the settings to the registers.
+mpu.configure()  # Apply the settings to the registers
+
+print("MPU9250 id: " + hex(mpu.whoami))
 
 while True:
-   
-    print("|.....MPU9250 in 0x68 I2C Bus - Master.....|")
     print("Accelerometer", mpu.readAccelerometerMaster())
     print("Gyroscope", mpu.readGyroscopeMaster())
     print("Magnetometer", mpu.readMagnetometerMaster())
     print("Temperature", mpu.readTemperatureMaster())
-    print("\n")
 
-    print("|.....MPU9250 in 0x68 I2C Bus - Slave in 0x68 auxiliary sensor address.....|")
-    print("Accelerometer", mpu.readAccelerometerSlave())
-    print("Gyroscope", mpu.readGyroscopeSlave())
-    print("Temperature", mpu.readTemperatureSlave())
-    print("\n")
-
-    time.sleep(1)
+    time.sleep(1)  # Sleep for 1 second
