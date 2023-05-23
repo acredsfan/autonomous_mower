7901f9201cfb27e8570cbeeade07d0273fe75cb2
acredsfan <acredsfan@gmail.com>
2023-05-23 11:19:50 -0400

    Update __init__.py

--- user_interface/__init__.py
+++ user_interface/__init__.py
@@ -1,1 +1,10 @@
-# init code for the user interface
+# init code for the web user interface
+# Path: user_interface\web_interface\__init__.py
+
+#IMPORTS
+from .web_interface import init_web_interface, start_web_interface
+
+__all__ = [
+    'init_web_interface',
+    'start_web_interface',
+]
