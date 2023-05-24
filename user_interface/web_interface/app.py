23e40ce1b93002044c422d584d2d8692afc8895a
acredsfan <acredsfan@gmail.com>
2023-05-23 20:41:12 -0400

    Update app.py

--- user_interface/web_interface/app.py
+++ user_interface/web_interface/app.py
@@ -18,9 +18,9 @@ Gst.init(None)
 sensor_data = "Sample sensor data"
 mowing_status = "Not mowing"
 next_scheduled_mow = "2023-05-06 12:00:00"
-live_view_url = "PiMowBot.local:8081"
+live_view_url = "PiMowBot.local:5000"
 google_maps_api_key = "YOUR_API_KEY_HERE"
-pipeline = Gst.parse_launch('v4l2src ! videoconvert ! x264enc speed-preset=ultrafast tune=zerolatency ! rtph264pay ! udpsink host=PiMowBot.local port=8081')
+pipeline = Gst.parse_launch('v4l2src ! videoconvert ! x264enc speed-preset=ultrafast tune=zerolatency ! rtph264pay ! udpsink host=PiMowBot.local port=5000')
 dotenv_path = os.path.join(os.path.dirname(__file__),'home' ,'pi', 'autonomous_mower', '.env')
 load_dotenv(dotenv_path)
 google_maps_api_key = os.getenv("GOOGLE_MAPS_API_KEY")
