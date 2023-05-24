cb47c9231aa13d5b5de47f4227006c9886281cb0
acredsfan <acredsfan@gmail.com>
2023-05-24 15:06:33 -0400

    Update test_tof.py

--- tests/test_tof.py
+++ tests/test_tof.py
@@ -1,26 +1,38 @@
-import sys
+# Python Code to Test the TOF Sensors
+
+# Import the Libraries
 import time
-sys.path.append('/home/pi/autonomous_mower')
-from hardware_interface.sensor_interface import SensorInterface
+import VL53L0X
 
-def main():
-    # Initialize the sensor interface
-    sensor_interface = SensorInterface()
+# Set up I2C multiplexer
+import smbus
+bus = smbus.SMBus(1)
+MUX_ADDRESS = 0x70  # Replace with your multiplexer's I2C address if different
 
-    # Initialize sensors
-    sensor_interface.init_sensors()
+# Set up the sensors
+# Right sensor
+right_tof=VL53L0X.VL53L0X(tca9548a_num=0, tca9548a_addr=0x70)
+right_tof.open()
+right_tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
 
-    while True:
-        # Read the distance from the left TOF sensor
-        left_distance = sensor_interface.read_vl53l0x_left()
-        print('Left Distance: ', left_distance)
+# Left sensor
+left_tof=VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
+left_tof.open()
+left_tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
 
-        # Read the distance from the right TOF sensor
-        right_distance = sensor_interface.read_vl53l0x_right()
-        print('Right Distance: ', right_distance)
-
-        # Wait for a second before next reading
-        time.sleep(1)
-
-if __name__ == '__main__':
-    main()
+# Read the sensors
+while True:
+    # Select the right sensor
+    bus.write_byte(MUX_ADDRESS, 1 << 3)
+    right_distance = right_tof.get_distance()
+    # Select the left sensor
+    bus.write_byte(MUX_ADDRESS, 1 << 2)
+    left_distance = left_tof.get_distance()
+    # Print the distances
+    print(f"Right distance: {right_distance}")
+    print(f"Left distance: {left_distance}")
+    # Wait for a bit before getting the next packet
+    time.sleep(1)
+# The output should look like this:
+# Right distance: 0
+# Left distance: 0
