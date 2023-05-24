66ca40d9e359948937afcad5545b0a68e466bcf5
acredsfan <acredsfan@gmail.com>
2023-05-24 15:23:09 -0400

    Update test_tof.py

--- tests/test_tof.py
+++ tests/test_tof.py
@@ -13,15 +13,15 @@ i2c = busio.I2C(board.SCL, board.SDA)
 # Create a VL53L0X object for device on TCA9548A bus 1
 tof_right = VL53L0X.VL53L0X(tca9548a_num=0, tca9548a_addr=0x70)
 # Create a VL53L0X object for device on TCA9548A bus 2
-tof_left = VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
+#tof_left = VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
 
 tof_right.open()
-tof_left.open()
+#tof_left.open()
 
 # Start ranging on TCA9548A bus 1
 tof_right.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 # Start ranging on TCA9548A bus 2
-tof_left.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+#tof_left.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
 timing = tof_right.get_timing()
 if timing < 20000:
@@ -35,14 +35,14 @@ for count in range(1, 101):
         print("1: %d mm, %d cm, %d" % (distance, (distance/10), count))
 
     # Get distance from VL53L0X  on TCA9548A bus 2
-    distance = tof_left.get_distance()
-    if distance > 0:
-        print("2: %d mm, %d cm, %d" % (distance, (distance/10), count))
+    #distance = tof_left.get_distance()
+    #if distance > 0:
+    #    print("2: %d mm, %d cm, %d" % (distance, (distance/10), count))
 
     time.sleep(timing/1000000.00)
 
 tof_right.stop_ranging()
-tof_left.stop_ranging()
+#tof_left.stop_ranging()
 
 tof_right.close()
-tof_left.close()
+#tof_left.close()
