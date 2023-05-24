b7d655f44ecd7e648e5fd16864d202129bd2976a
acredsfan <acredsfan@gmail.com>
2023-05-24 15:15:14 -0400

    Update test_tof.py

--- tests/test_tof.py
+++ tests/test_tof.py
@@ -4,33 +4,38 @@
 import time
 import VL53L0X
 
-# Set up I2C multiplexer
-import smbus
-bus = smbus.SMBus(1)
-MUX_ADDRESS = 0x70  # Replace with your multiplexer's I2C address if different
+# Create a VL53L0X object for device on TCA9548A bus 1
+tof1 = VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
+# Create a VL53L0X object for device on TCA9548A bus 2
+tof2 = VL53L0X.VL53L0X(tca9548a_num=2, tca9548a_addr=0x70)
+tof1.open()
+tof2.open()
 
-# Set up the sensors
-# Right sensor
-right_tof=VL53L0X.VL53L0X(tca9548a_num=0, tca9548a_addr=0x70)
-right_tof.open()
-right_tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+# Start ranging on TCA9548A bus 1
+tof1.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+# Start ranging on TCA9548A bus 2
+tof2.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
-# Left sensor
-left_tof=VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
-left_tof.open()
-left_tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+timing = tof1.get_timing()
+if timing < 20000:
+    timing = 20000
+print("Timing %d ms" % (timing/1000))
 
-# Read the sensors
-while True:
-    # Select the right sensor
-    right_distance = right_tof.get_distance()
-    # Select the left sensor
-    left_distance = left_tof.get_distance()
-    # Print the distances
-    print(f"Right distance: {right_distance}")
-    print(f"Left distance: {left_distance}")
-    # Wait for a bit before getting the next packet
-    time.sleep(1)
-# The output should look like this:
-# Right distance: 0
-# Left distance: 0
+for count in range(1, 101):
+    # Get distance from VL53L0X  on TCA9548A bus 1
+    distance = tof1.get_distance()
+    if distance > 0:
+        print("1: %d mm, %d cm, %d" % (distance, (distance/10), count))
+
+    # Get distance from VL53L0X  on TCA9548A bus 2
+    distance = tof2.get_distance()
+    if distance > 0:
+        print("2: %d mm, %d cm, %d" % (distance, (distance/10), count))
+
+    time.sleep(timing/1000000.00)
+
+tof1.stop_ranging()
+tof2.stop_ranging()
+
+tof1.close()
+tof2.close()
