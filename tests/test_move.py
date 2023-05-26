9fda005fc65450ebedb99d89216ae2f5aeba0ad0
acredsfan <acredsfan@gmail.com>
2023-05-26 19:48:42 -0400

    Update test_move.py

--- tests/test_move.py
+++ tests/test_move.py
@@ -18,6 +18,7 @@ right_motor = None
 relay_controller_state = False
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 # Set up GPIO
 GPIO.setup(SPEED_CONTROLLER_PIN, GPIO.OUT)
@@ -32,59 +33,74 @@ GPIO.setup(RIGHT_IN4_PIN, GPIO.OUT)
 left_motor = GPIO.PWM(LEFT_PWMI_PIN, 100)
 right_motor = GPIO.PWM(RIGHT_PWMI_PIN, 100)
 
-left_motor.start(0)
-right_motor.start(0)
+def init_motor_controller():
+    left_motor.start(0)
+    right_motor.start(0)
 
-# Test speed controller
-print("Testing speed controller...")
-time.sleep(10)
-GPIO.output(SPEED_CONTROLLER_PIN, GPIO.HIGH)
-print("Speed controller on")
+def relay_on():
+    GPIO.output(SPEED_CONTROLLER_PIN, GPIO.HIGH)
+    print("Speed controller on")
 
-# Move both forward
-print("Moving both forward...")
-GPIO.output(LEFT_IN1_PIN, GPIO.HIGH)
-GPIO.output(LEFT_IN2_PIN, GPIO.LOW)
-GPIO.output(RIGHT_IN3_PIN, GPIO.HIGH)
-GPIO.output(RIGHT_IN4_PIN, GPIO.LOW)
-left_motor.ChangeDutyCycle(50)
-right_motor.ChangeDutyCycle(50)
-time.sleep(2)
+def relay_off():
+    GPIO.output(SPEED_CONTROLLER_PIN, GPIO.LOW)
+    print("Speed controller off")
 
-# Move both backward
-print("Moving both backward...")
-GPIO.output(LEFT_IN1_PIN, GPIO.LOW)
-GPIO.output(LEFT_IN2_PIN, GPIO.HIGH)
-GPIO.output(RIGHT_IN3_PIN, GPIO.LOW)
-GPIO.output(RIGHT_IN4_PIN, GPIO.HIGH)
-left_motor.ChangeDutyCycle(100)
-right_motor.ChangeDutyCycle(100)
-time.sleep(2)
+def set_motor_speed(left_speed, right_speed):
+    left_motor.ChangeDutyCycle(left_speed)
+    right_motor.ChangeDutyCycle(right_speed)
 
-# Turn Right (Left forward, Right backward)
-print("Turning right...")
-GPIO.output(LEFT_IN1_PIN, GPIO.HIGH)
-GPIO.output(LEFT_IN2_PIN, GPIO.LOW)
-GPIO.output(RIGHT_IN3_PIN, GPIO.LOW)
-GPIO.output(RIGHT_IN4_PIN, GPIO.HIGH)
-left_motor.ChangeDutyCycle(100)
-right_motor.ChangeDutyCycle(100)
-time.sleep(2)
+def set_motor_direction(direction):
+    if direction == "forward":
+        GPIO.output(LEFT_IN1_PIN, GPIO.HIGH)
+        GPIO.output(LEFT_IN2_PIN, GPIO.LOW)
+        GPIO.output(RIGHT_IN3_PIN, GPIO.HIGH)
+        GPIO.output(RIGHT_IN4_PIN, GPIO.LOW)
+    elif direction == "backward":
+        GPIO.output(LEFT_IN1_PIN, GPIO.LOW)
+        GPIO.output(LEFT_IN2_PIN, GPIO.HIGH)
+        GPIO.output(RIGHT_IN3_PIN, GPIO.LOW)
+        GPIO.output(RIGHT_IN4_PIN, GPIO.HIGH)
+    elif direction == "left":
+        GPIO.output(LEFT_IN1_PIN, GPIO.LOW)
+        GPIO.output(LEFT_IN2_PIN, GPIO.HIGH)
+        GPIO.output(RIGHT_IN3_PIN, GPIO.HIGH)
+        GPIO.output(RIGHT_IN4_PIN, GPIO.LOW)
+    elif direction == "right":
+        GPIO.output(LEFT_IN1_PIN, GPIO.HIGH)
+        GPIO.output(LEFT_IN2_PIN, GPIO.LOW)
+        GPIO.output(RIGHT_IN3_PIN, GPIO.LOW)
+        GPIO.output(RIGHT_IN4_PIN, GPIO.HIGH)
+    elif direction == "stop":
+        GPIO.output(LEFT_IN1_PIN, GPIO.LOW)
+        GPIO.output(LEFT_IN2_PIN, GPIO.LOW)
+        GPIO.output(RIGHT_IN3_PIN, GPIO.LOW)
+        GPIO.output(RIGHT_IN4_PIN, GPIO.LOW)
+    else:
+        print("Invalid direction")
 
-# Turn Left (Left backward, Right forward)
-print("Turning left...")
-GPIO.output(LEFT_IN1_PIN, GPIO.LOW)
-GPIO.output(LEFT_IN2_PIN, GPIO.HIGH)
-GPIO.output(RIGHT_IN3_PIN, GPIO.HIGH)
-GPIO.output(RIGHT_IN4_PIN, GPIO.LOW)
-left_motor.ChangeDutyCycle(100)
-right_motor.ChangeDutyCycle(100)
-time.sleep(2)
+def test_motor_controller():
+    init_motor_controller()
+    print("Testing motor controller...")
+    relay_on()
+    print("Speed controller on")
+    set_motor_speed(100, 100)
+    print("Speed set to 100")
+    set_motor_direction("forward")
+    print("Direction set to forward")
+    time.sleep(5)
+    set_motor_direction("backward")
+    print("Direction set to backward")
+    time.sleep(5)
+    set_motor_direction("left")
+    print("Direction set to left")
+    time.sleep(5)
+    set_motor_direction("right")
+    print("Direction set to right")
+    time.sleep(5)
+    set_motor_direction("stop")
+    print("Direction set to stop")
+    relay_off()
+    print("Speed controller off")
+    GPIO.cleanup()
 
-# Turn off Relay
-print("Turning off speed controller...")
-GPIO.output(SPEED_CONTROLLER_PIN, GPIO.LOW)
-print("Speed controller off")
-
-# Clean up GPIO
-GPIO.cleanup()
+test_motor_controller()
