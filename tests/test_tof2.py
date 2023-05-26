58cb73e47c6d6482d622c1d235cfc7ce937ae71d
acredsfan <acredsfan@gmail.com>
2023-05-26 18:03:42 -0400

    Update test_tof2.py

--- tests/test_tof2.py
+++ tests/test_tof2.py
@@ -1,34 +1,10 @@
-#!/usr/bin/python
-
-# MIT License
-# 
-# Copyright (c) 2017 John Bryan Moore
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import time
 import VL53L0X
 
 # Create a VL53L0X object for device on TCA9548A bus 1
-tof1 = VL53L0X.VL53L0X(tca9548a_num=0, tca9548a_addr=0x70)
+tof1 = VL53L0X.VL53L0X(tca9548a_num=4, tca9548a_addr=0x70)
 # Create a VL53L0X object for device on TCA9548A bus 2
-tof2 = VL53L0X.VL53L0X(tca9548a_num=1, tca9548a_addr=0x70)
+tof2 = VL53L0X.VL53L0X(tca9548a_num=5, tca9548a_addr=0x70)
 tof1.open()
 tof2.open()
 
