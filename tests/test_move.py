bcdfa2c1a76bf714d1675b7b0232888dc594890d
acredsfan <acredsfan@gmail.com>
2023-05-26 19:39:27 -0400

    Update test_move.py

--- tests/test_move.py
+++ tests/test_move.py
@@ -38,6 +38,9 @@ right_motor.start(0)
 
 # Test speed controller
 print("Testing speed controller...")
+wait_time = 10
+GPIO.output(SPEED_CONTROLLER_PIN, GPIO.LOW)
+time.sleep(wait_time)
 GPIO.output(SPEED_CONTROLLER_PIN, GPIO.HIGH)
 print("Speed controller on")
 
