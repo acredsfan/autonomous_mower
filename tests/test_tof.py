19c14acdb9b5b39702e29bf90ade8b22d3241c24
acredsfan <acredsfan@gmail.com>
2023-05-26 18:04:56 -0400

    Update test_tof.py

--- tests/test_tof.py
+++ tests/test_tof.py
@@ -1,13 +1,13 @@
 import time
-import VL53L0X
+from VL53L0X import VL53L0X
 from smbus2 import SMBus, i2c_msg
 
 # I2C multiplexer address
 MUX_ADDRESS = 0x70
 
 # ToF sensor addresses on the multiplexer
-TOF_RIGHT_CHANNEL = 4
-TOF_LEFT_CHANNEL = 5
+TOF_RIGHT_CHANNEL = 0
+TOF_LEFT_CHANNEL = 1
 
 # I2C bus number (usually 1 for Raspberry Pi)
 I2C_BUS = 1
@@ -20,27 +20,17 @@ def select_channel(channel):
 
 def read_sensor(channel):
     select_channel(channel)
-    tof = VL53L0X.VL53L0X(i2c_bus=I2C_BUS, i2c_address=0x29)
-    success = tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
-    if not success:
-        print("Error starting ranging")
-        return None
+    tof = VL53L0X(i2c_bus=I2C_BUS, i2c_address=0x29)
+    tof.start_ranging(VL53L0X.BETTER_ACCURACY_MODE)
     distance = tof.get_distance() # This is in millimeters
     tof.stop_ranging()
     return distance / 25.4  # Convert to inches
 
-def main():
-    distance_right = None
-    distance_left = None
+while True:
+    distance_right = read_sensor(TOF_RIGHT_CHANNEL)
+    print(f"Right ToF Sensor Distance: {distance_right} inches")
+    time.sleep(0.1)  # You can adjust this delay as needed
 
-    while True:
-        distance_right = read_sensor(TOF_RIGHT_CHANNEL)
-        print(f"Right ToF Sensor Distance: {distance_right} inches")
-        time.sleep(0.1)  # You can adjust this delay as needed
-
-        distance_left = read_sensor(TOF_LEFT_CHANNEL)
-        print(f"Left ToF Sensor Distance: {distance_left} inches")
-        time.sleep(0.1)  # You can adjust this delay as needed
-
-if __name__ == "__main__":
-    main()
+    distance_left = read_sensor(TOF_LEFT_CHANNEL)
+    print(f"Left ToF Sensor Distance: {distance_left} inches")
+    time.sleep(0.1)  # You can adjust this delay as needed
