0197af7cdaac3a5efb7f2d2a80d7f0a27d0e29ee
acredsfan <acredsfan@gmail.com>
2023-05-27 13:33:53 -0400

    Update relay_controller.py

--- hardware_interface/relay_controller.py
+++ hardware_interface/relay_controller.py
@@ -14,11 +14,13 @@ relay_controller_state = False
 class RelayController:
 #init_relay_controller
     #initializes the relay controller
+    @staticmethod
     def init_relay_controller():
         GPIO.setup(SPEED_CONTROLLER_PIN, GPIO.OUT)
 
     #toggle_speed_controller
     #toggles the speed controller connected to GPIO 5
+    @staticmethod
     def toggle_speed_controller():
         global relay_controller_state
         if relay_controller_state == False:
@@ -30,6 +32,7 @@ class RelayController:
 
     #set_speed_controller
     #sets the speed controller connected to GPIO 5
+    @staticmethod
     def set_speed_controller(state):
         global relay_controller_state
         if state == "on":
@@ -41,11 +44,13 @@ class RelayController:
 
     #get_speed_controller_state
     #returns the state of the speed controller connected to GPIO 5
+    @staticmethod
     def get_speed_controller_state():
         global relay_controller_state
         return relay_controller_state
     
     #clean_up
     #cleans up the GPIO pins
+    @staticmethod
     def clean_up():
         GPIO.cleanup()
