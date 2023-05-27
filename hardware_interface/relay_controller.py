3375cd7b8370793acb342222a29cb61f8185cc8d
acredsfan <acredsfan@gmail.com>
2023-05-27 13:22:52 -0400

    Update relay_controller.py

--- hardware_interface/relay_controller.py
+++ hardware_interface/relay_controller.py
@@ -6,11 +6,9 @@ import time
 
 #CONSTANTS
 SPEED_CONTROLLER_PIN = 5
-MOWER_BLADES_PIN = 6
 
 #VARIABLES
 relay_controller_state = False
-mower_blades_state = False
 
 #FUNCTIONS
 class RelayController:
@@ -18,18 +16,6 @@ class RelayController:
     #initializes the relay controller
     def init_relay_controller():
         GPIO.setup(SPEED_CONTROLLER_PIN, GPIO.OUT)
-        GPIO.setup(MOWER_BLADES_PIN, GPIO.OUT)
-
-    #toggle_mower_blades
-    #toggles the mower blades connected to GPIO 6
-    def toggle_mower_blades():
-        global mower_blades_state
-        if mower_blades_state == False:
-            GPIO.output(MOWER_BLADES_PIN, GPIO.HIGH)
-            mower_blades_state = True
-        else:
-            GPIO.output(MOWER_BLADES_PIN, GPIO.LOW)
-            mower_blades_state = False
 
     #toggle_speed_controller
     #toggles the speed controller connected to GPIO 5
@@ -53,15 +39,13 @@ class RelayController:
             GPIO.output(SPEED_CONTROLLER_PIN, GPIO.LOW)
             relay_controller_state = False
 
-    #set_mower_blades
-    #sets the mower blades connected to GPIO 6
-    def set_mower_blades(state):
-        global mower_blades_state
-        if state == "on":
-            GPIO.output(MOWER_BLADES_PIN, GPIO.HIGH)
-            mower_blades_state = True
-        else:
-            GPIO.output(MOWER_BLADES_PIN, GPIO.LOW)
-            mower_blades_state = False
-
-    #END OF FILE
+    #get_speed_controller_state
+    #returns the state of the speed controller connected to GPIO 5
+    def get_speed_controller_state():
+        global relay_controller_state
+        return relay_controller_state
+    
+    #clean_up
+    #cleans up the GPIO pins
+    def clean_up():
+        GPIO.cleanup()
