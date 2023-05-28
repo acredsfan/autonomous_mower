e55285aeb01af956ef0fea0f0dccf7ab4f0b9ab1
acredsfan <acredsfan@gmail.com>
2023-05-28 10:51:44 -0400

    Update sensor_interface.py

--- hardware_interface/sensor_interface.py
+++ hardware_interface/sensor_interface.py
@@ -124,7 +124,7 @@ class SensorInterface:
     def read_ina3221(self):
         """Read INA3221 power monitor data."""
         try:
-            return self.ina3221.bus_voltage(1)
+            return self.ina3221.bus_voltage()
         except Exception as e:
             print(f"Error during INA3221 read: {e}")
 
@@ -160,3 +160,6 @@ class SensorInterface:
 
         # Return True if all conditions are met
         return True
+    
+# At the end of sensor_interface.py
+sensor_interface = SensorInterface()
