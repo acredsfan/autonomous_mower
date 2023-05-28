a2079f0877da1370bdc1a98ff52ee29e13301770
acredsfan <acredsfan@gmail.com>
2023-05-28 09:31:09 -0400

    Update test_worm_motors.py

--- hardware_interface/test_worm_motors.py
+++ hardware_interface/test_worm_motors.py
@@ -1,37 +1,60 @@
 #Script to test Worm Motors
-
-#IMPORTS
-import worm_motors
-import relay_controller
+import RPi.GPIO as GPIO
 import time
 
-#CONSTANTS
-SPEED = 100
+# Set up the GPIO Pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 
-# Turn on relay controller
-#relay_controller.RelayController.init()
-#relay_controller.RelayController.set_speed_controller("on")
+# Set up the PWM channels
+pwm_A = GPIO.PWM(13, 100)
+pwm_B = GPIO.PWM(16, 100)
 
-# Test worm motors
-print("Testing worm motors...")
-worm_motors.MotorController.move_mower("forward", SPEED)
-print("moving forward")
-time.sleep(2)
-worm_motors.MotorController.move_mower("backward", SPEED)
-print("moving backward")
-time.sleep(2)
-worm_motors.MotorController.move_mower("left", SPEED)
-print("moving left")
-time.sleep(2)
-worm_motors.MotorController.move_mower("right", SPEED)
-print("moving right")
-time.sleep(2)
-worm_motors.MotorController.stop_motors()
-print("Worm motors off")
+def set_speed(speed_A, speed_B):
+    try:
+        # Set the duty cycle for each PWM channel
+        pwm_A.start(speed_A)
+        pwm_B.start(speed_B)
+    except Exception as e:
+        print("An error occurred while setting the speed: ", str(e))
 
-# Clean up worm motors
-worm_motors.MotorController.cleanup()
+def set_direction(direction_A, direction_B):
+    try:
+        # Set the motor direction for Motor A
+        if direction_A == 'FORWARD':
+            GPIO.output(19, GPIO.HIGH)
+            GPIO.output(26, GPIO.LOW)
+        else:
+            GPIO.output(19, GPIO.LOW)
+            GPIO.output(26, GPIO.HIGH)
 
-# Turn off relay controller
-#relay_controller.RelayController.set_speed_controller("off")
-#relay_controller.RelayController.clean_up()
+        # Set the motor direction for Motor B
+        if direction_B == 'FORWARD':
+            GPIO.output(20, GPIO.HIGH)
+            GPIO.output(21, GPIO.LOW)
+        else:
+            GPIO.output(20, GPIO.LOW)
+            GPIO.output(21, GPIO.HIGH)
+    except Exception as e:
+        print("An error occurred while setting the direction: ", str(e))
+
+def cleanup():
+    # Stop the motors
+    pwm_A.stop()
+    pwm_B.stop()
+
+    # Reset the GPIO pins
+    GPIO.cleanup()
+
+# Test the motors
+try:
+    set_speed(50, 50)  # Set speed for both motors to 50%
+    set_direction('FORWARD', 'FORWARD')  # Set both motors to move forward
+    time.sleep(5)  # Run the motors for 5 seconds
+finally:
+    cleanup()
