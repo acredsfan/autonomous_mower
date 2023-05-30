21387e63f03214f8900baa2655b66f68d9f089d1
acredsfan <acredsfan@gmail.com>
2023-05-30 15:55:17 -0400

    Update camera.py

--- user_interface/web_interface/camera.py
+++ user_interface/web_interface/camera.py
@@ -5,7 +5,21 @@ class VideoCamera(object):
         # Using OpenCV to capture from device 0. If you have trouble capturing
         # from a webcam, comment the line below out and use a video file
         # instead.
-        self.video = cv2.VideoCapture(0)
+        self.capture_width = 640
+        self.capture_height = 480
+        self.display_width = 640
+        self.display_height = 480
+        self.framerate = 90
+
+        self.gstreamer_pipeline = (
+            f"nvarguscamerasrc ! video/x-raw(memory:NVMM), "
+            f"width=(int){self.capture_width}, height=(int){self.capture_height}, "
+            f"format=(string)NV12, framerate=(fraction){self.framerate}/1 ! "
+            f"nvvidconv flip-method=0 ! video/x-raw, width=(int){self.display_width}, height=(int){self.display_height}, format=(string)BGRx ! "
+            f"videoconvert ! video/x-raw, format=(string)BGR ! appsink"
+        )
+
+        self.video = cv2.VideoCapture(self.gstreamer_pipeline, cv2.CAP_GSTREAMER)
     
     def __del__(self):
         self.video.release()
