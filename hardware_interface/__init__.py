4f792f6fb6c86c9eef006bfbdd502873ad6b5914
acredsfan <acredsfan@gmail.com>
2023-06-01 12:07:18 -0400

    Update __init__.py

--- hardware_interface/__init__.py
+++ hardware_interface/__init__.py
@@ -4,9 +4,11 @@
 from .motor_controller import MotorController
 from .relay_controller import RelayController
 from .sensor_interface import SensorInterface
+from .blade_controller import BladeController
 
 __all__ = [
     'MotorController',
     'RelayController',
-    'SensorInterface'
+    'SensorInterface',
+    'BladeController'
 ]
