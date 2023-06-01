31fc7f2b8647cebd7bb5eb5ac87dc609ba16de7a
acredsfan <acredsfan@gmail.com>
2023-06-01 11:43:21 -0400

    Update Camera Streaming

--- user_interface/web_interface/camera.py
+++ user_interface/web_interface/camera.py
@@ -2,28 +2,13 @@ import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
-        self.capture_width = 640
-        self.capture_height = 480
-        self.display_width = 640
-        self.display_height = 480
-        self.framerate = 90
-
-        self.gstreamer_pipeline = (
-            f"nvarguscamerasrc ! video/x-raw(memory:NVMM), "
-            f"width=(int){self.capture_width}, height=(int){self.capture_height}, "
-            f"format=(string)NV12, framerate=(fraction){self.framerate}/1 ! "
-            f"nvvidconv flip-method=0 ! video/x-raw, width=(int){self.display_width}, height=(int){self.display_height}, format=(string)BGRx ! "
-            f"videoconvert ! video/x-raw, format=(string)BGR ! appsink"
-        )
-
-        self.video = cv2.VideoCapture(self.gstreamer_pipeline, cv2.CAP_GSTREAMER)
+        # Use OpenCV to caputure from Raspberry Pi camera module
+        self.video = cv2.VideoCapture(0)
     
-    def __del__(self):
+def __del__(self):  
+    if hasattr(self, 'video'):
         self.video.release()
-    
+
     def get_frame(self):
         success, image = self.video.read()
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
--- user_interface/web_interface/test_camera.py
+++ user_interface/web_interface/test_camera.py
@@ -2,23 +2,40 @@ import cv2
 from camera import VideoCamera
 
 def test_video_camera():
-    camera = VideoCamera()
-    success, frame = camera.video.read()
+    try:
+        camera = VideoCamera()
+    except Exception as e:
+        print("Failed to initialize VideoCamera. Error: ", str(e))
+        return
+
+    try:
+        success, frame = camera.video.read()
+    except Exception as e:
+        print("Failed to read frame from camera. Error: ", str(e))
+        return
 
     if not success:
         print("Failed to read frame from camera.")
         return
 
-    ret, jpeg = cv2.imencode('.jpg', frame)
+    try:
+        ret, jpeg = cv2.imencode('.jpg', frame)
+    except Exception as e:
+        print("Failed to encode frame as JPEG. Error: ", str(e))
+        return
 
     if not ret:
         print("Failed to encode frame as JPEG.")
         return
 
     # Display the frame to ensure it's captured correctly
-    cv2.imshow("Frame", frame)
-    cv2.waitKey(0)  # Wait for any key press
-    cv2.destroyAllWindows()
+    try:
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(0)  # Wait for any key press
+        cv2.destroyAllWindows()
+    except Exception as e:
+        print("Failed to display frame. Error: ", str(e))
+        return
 
 if __name__ == "__main__":
     test_video_camera()
