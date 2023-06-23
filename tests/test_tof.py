0d7d215dde0b8f01466267de79e2b7e9eb2ba607
acredsfan <acredsfan@gmail.com>
2023-06-23 10:20:37 -0400

    Update test_tof.py

--- tests/test_tof.py
+++ tests/test_tof.py
@@ -7,18 +7,18 @@ try:
     # GPIO for Sensor 1 shutdown pin
     right_shutdown = 23
     # GPIO for Sensor 2 shutdown pin
-    left_shutdown = 22
+    #left_shutdown = 22
 
     GPIO.setwarnings(False)
 
     # Setup GPIO for shutdown pins on each VL53L0X
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(right_shutdown, GPIO.OUT)
-    GPIO.setup(left_shutdown, GPIO.OUT)
+    #GPIO.setup(left_shutdown, GPIO.OUT)
 
     # Set all shutdown pins low to turn off each VL53L0X
     GPIO.output(right_shutdown, GPIO.LOW)
-    GPIO.output(left_shutdown, GPIO.LOW)
+    #GPIO.output(left_shutdown, GPIO.LOW)
 
     # Keep all low for 500 ms or so to make sure they reset
     time.sleep(0.50)
@@ -26,10 +26,10 @@ try:
     # Create a VL53L0X object for device on TCA9548A bus 1
     tof_right = VL53L0X.VL53L0X(tca9548a_num=6, tca9548a_addr=0x70)
     # Create a VL53L0X object for device on TCA9548A bus 2
-    tof_left = VL53L0X.VL53L0X(tca9548a_num=7, tca9548a_addr=0x70)
+    #tof_left = VL53L0X.VL53L0X(tca9548a_num=7, tca9548a_addr=0x70)
     tof_right.open()
-    tof_left.change_address(0X30)
-    tof_left.open()
+    #tof_left.change_address(0X30)
+    #tof_left.open()
 
     # Start ranging on TCA9548A bus 1
     GPIO.output(right_shutdown, GPIO.HIGH)
@@ -39,7 +39,7 @@ try:
     GPIO.output(left_shutdown, GPIO.HIGH)
     time.sleep(0.50)
 
-    tof_left.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+    #tof_left.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
     timing = tof_right.get_timing()
     if timing < 20000:
@@ -55,21 +55,21 @@ try:
             print("%d - ERROR" % 1)
 
         # Get distance from VL53L0X  on TCA9548A bus 2
-        distance = tof_left.get_distance()
-        if distance > 0:
-            print("left: %d mm, %d cm, %d" % (distance, (distance/10), count))
-        else:
-            print("%d - ERROR" % 2)
+        # distance = tof_left.get_distance()
+        # if distance > 0:
+        #     print("left: %d mm, %d cm, %d" % (distance, (distance/10), count))
+        # else:
+        #     print("%d - ERROR" % 2)
 
         time.sleep(timing/1000000.00)
 
     tof_right.stop_ranging()
     GPIO.output(right_shutdown, GPIO.LOW)
-    tof_left.stop_ranging()
-    GPIO.output(left_shutdown, GPIO.LOW)
+    #tof_left.stop_ranging()
+    #GPIO.output(left_shutdown, GPIO.LOW)
 
     tof_right.close()
-    tof_left.close()
+    #tof_left.close()
 
 
 except KeyboardInterrupt:
