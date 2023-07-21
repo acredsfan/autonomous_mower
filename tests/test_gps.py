944f92f37fde9578be1fc31e4a6b98bd657fc23e
acredsfan <acredsfan@gmail.com>
2023-07-21 13:17:32 -0400

    Update test_gps.py

--- tests/test_gps.py
+++ tests/test_gps.py
@@ -3,7 +3,7 @@ import gpsd
 
 class GPSInterface:
     def __init__(self, port='/dev/serial0', baud_rate=9600, timeout=1):
-        self.serial = serial.Serial(port, baud_rate, timeout)
+        self.serial = serial.Serial(port=port, baudrate=baud_rate, timeout=timeout)
         self.serial.flush()
         self.gpsd = gpsd.GPSD(port=port, baudrate=baud_rate, timeout=timeout)
 
