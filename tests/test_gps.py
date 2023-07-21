9a1e0f539a3feadb0a6c0c31aa3e5e190a0890d4
acredsfan <acredsfan@gmail.com>
2023-07-21 13:12:36 -0400

    Update test_gps.py

--- tests/test_gps.py
+++ tests/test_gps.py
@@ -1,8 +1,32 @@
-import gpsd
-import time
+import serial
+from gpsd import gpsd
 
-# Connect to the local gpsd
-gpsd.connect()
+class GPSInterface:
+    def __init__(self, port='/dev/serial0', baud_rate=9600, timeout=1):
+        self.serial = serial.Serial(port, baud_rate, timeout)
+        self.serial.flush()
+        self.gpsd = gpsd.GPSD(port=port, baudrate=baud_rate, timeout=timeout)
+
+    def read_gps_data(self):
+        data = self.gpsd.next()
+        if data['class'] == 'TPV':
+            return {
+                'latitude': data['lat'],
+                'longitude': data['lon'],
+                'altitude': data['alt'],
+                'speed': data['speed'],
+                'timestamp': data['time'],
+                'satellites': data['satellites'],
+                'quality': data['fix'],
+                'pdop': data['pdop'],
+                'hdop': data['hdop'],
+                'vdop': data['vdop'],
+            }
+        else:
+            return None
+
+    def close(self):
+        self.gpsd.close()
 
 while True:
     # Get gps position
