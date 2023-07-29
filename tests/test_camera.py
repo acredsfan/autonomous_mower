96b7dde2898a5b32bee30873c335a896cf37c4f9
acredsfan <acredsfan@gmail.com>
2023-07-29 14:56:00 -0400

    Update test_camera.py

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,12 +1,18 @@
 import cv2
 
-cap = cv2.VideoCapture(0)  # Change the number to test other devices
+try:
+    # cap = cv2.VideoCapture(1)  # Change the number to test other devices
+    cap = cv2.VideoCapture('v4l2src device=/dev/video0 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 
-ret, frame = cap.read()
+    ret, frame = cap.read()
 
-if ret:
-    cv2.imwrite('test_image.jpg', frame)
-else:
-    print('Failed to capture image')
+    if ret:
+        cv2.imwrite('test_image.jpg', frame)
+        print('Image captured successfully.')
+    else:
+        print('Failed to capture image')
+        
+    cap.release()
 
-cap.release()
+except KeyboardInterrupt:
+    print('Aborted by user (Ctrl + C).')
