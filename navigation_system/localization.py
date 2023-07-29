ff323cac59b3279420fdf030712d06c6d98f8341
acredsfan <acredsfan@gmail.com>
2023-07-29 12:32:09 -0400

    Update localization.py

--- navigation_system/localization.py
+++ navigation_system/localization.py
@@ -1,12 +1,8 @@
-# This module is responsible for estimating the robot's position and orientation within its environment. 
-# It will take sensor data, such as from the GPS module and the MPU-9250 Compass Module, and use it to compute the robot's location and heading. 
-# This may involve implementing sensor fusion algorithms, such as a Kalman filter or a particle filter, to combine the data from multiple sensors and 
-# obtain a more accurate estimate of the robot's pose.
-
-# localization.py
+# Import required modules
 import math
+import time
 from hardware_interface import sensor_interface
-import gpsd
+from gps_interface import GPSInterface  # Import the GPSInterface from gps_interface.py
 
 # Global variables
 current_latitude = 0
@@ -16,7 +12,8 @@ current_heading = 0
 
 class Localization:
     def __init__(self, port='/dev/serial0', baud_rate=9600, timeout=1):
-        self.gpsd = gpsd.GPSD(port=port, baudrate=baud_rate, timeout=timeout)
+        # Replace gpsd with GPSInterface
+        self.gps = GPSInterface(port=port, baudrate=baud_rate, timeout=timeout)
 
     def gps_to_meters(self, lat1, lon1, lat2, lon2):
         """
@@ -34,16 +31,15 @@ class Localization:
         distance = R * c
         return distance
 
-
     def estimate_position(self):
         global current_latitude, current_longitude, current_altitude
 
-        data = self.gpsd.next()
-        if data['class'] == 'TPV':
+        # Use GPSInterface to get the GPS data
+        data = self.gps.get_data()
+        if data is not None:
             current_latitude = data['lat']
             current_longitude = data['lon']
             current_altitude = data['alt']
-
 
     def estimate_orientation(self):
         global current_heading
@@ -57,27 +53,24 @@ class Localization:
         except Exception as e:
             print(f"Error estimating orientation: {e}")
 
-
     def get_current_position(self):
         return current_latitude, current_longitude, current_altitude
 
-
     def get_current_orientation(self):
         return current_heading
-
 
     def update(self):
         self.estimate_position()
         self.estimate_orientation()
 
 
-    if __name__ == '__main__':
-        localization = Localization()
+if __name__ == '__main__':
+    localization = Localization()
 
-        while True:
-            localization.update()
-            lat, lon, alt = localization.get_current_position()
-            heading = localization.get_current_orientation()
+    while True:
+        localization.update()
+        lat, lon, alt = localization.get_current_position()
+        heading = localization.get_current_orientation()
 
-            print(f"Latitude: {lat}, Longitude: {lon}, Altitude: {alt}, Heading: {heading}")
-            time.sleep(1)
+        print(f"Latitude: {lat}, Longitude: {lon}, Altitude: {alt}, Heading: {heading}")
+        time.sleep(1)
