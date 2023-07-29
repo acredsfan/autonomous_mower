69809726c79f5cbe5372ba7f365e8ec162703229
acredsfan <acredsfan@gmail.com>
2023-07-29 15:05:06 -0400

    Update camera.py

--- user_interface/web_interface/camera.py
+++ user_interface/web_interface/camera.py
@@ -1,18 +1,18 @@
-import cv2  
+import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        # Use OpenCV to caputure from Raspberry Pi camera module  
-        self.cap = cv2.VideoCapture(0)
+        # Use OpenCV to capture from Raspberry Pi camera module
+        self.cap = cv2.VideoCapture('v4l2src device=/dev/video0 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
 
-    def __del__(self):   
-        if hasattr(self, 'cap'):  
-            self.cap.release()  
+    def __del__(self):
+        if hasattr(self, 'cap'):
+            self.cap.release()
 
-    def get_frame(self):  
-        success, image = self.cap.read()  
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,  
-        # so we must encode it into JPEG in order to correctly display the  
-        # video stream.  
-        ret, jpeg = cv2.imencode('.jpg', image)  
+    def get_frame(self):
+        success, image = self.cap.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
