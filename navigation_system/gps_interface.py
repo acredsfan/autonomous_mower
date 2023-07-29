785b08e9ca8de1fb0e50d893bf659ef1422a57ea
acredsfan <acredsfan@gmail.com>
2023-07-29 11:41:11 -0400

    update GPS to use GPSD

--- navigation_system/gps_interface.py
+++ navigation_system/gps_interface.py
@@ -1,30 +1,23 @@
-import serial
 import gpsd
+import time
 
 class GPSInterface:
-    def __init__(self, port='/dev/serial0', baud_rate=9600, timeout=1):
-        self.serial = serial.Serial(port, baud_rate, timeout)
-        self.serial.flush()
-        self.gpsd = gpsd.GPSD(port=port, baudrate=baud_rate, timeout=timeout)
+    def __init__(self):
+        gpsd.connect()
 
     def read_gps_data(self):
-        data = self.gpsd.next()
-        if data['class'] == 'TPV':
+        packet = gpsd.get_current()
+        if packet.mode >= 2:  # 2D or 3D fix
             return {
-                'latitude': data['lat'],
-                'longitude': data['lon'],
-                'altitude': data['alt'],
-                'speed': data['speed'],
-                'timestamp': data['time'],
-                'satellites': data['satellites'],
-                'quality': data['fix'],
-                'pdop': data['pdop'],
-                'hdop': data['hdop'],
-                'vdop': data['vdop'],
+                'latitude': packet.lat,
+                'longitude': packet.lon,
+                'altitude': packet.alt,
+                'speed': packet.hspeed,  # Horizontal speed
+                'timestamp': packet.time,
+                'mode': packet.mode,
             }
         else:
             return None
 
     def close(self):
         self.gpsd.close()
-
