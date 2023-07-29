853f0a0ee058ec4fed22675a6a983107ec94ccec
acredsfan <acredsfan@gmail.com>
2023-07-29 11:28:20 -0400

    Update test_gps.py

--- tests/test_gps.py
+++ tests/test_gps.py
@@ -1,42 +1,14 @@
-import serial
 import gpsd
+import time
 
-class GPSInterface:
-    def __init__(self, port='/dev/serial0', baud_rate=9600, timeout=1):
-        self.serial = serial.Serial(port=port, baudrate=baud_rate, timeout=timeout)
-        self.serial.flush()
-        self.gpsd = gpsd.GPSD(port=port, baudrate=baud_rate, timeout=timeout)
+# Connect to the local gpsd
+gpsd.connect()
 
-    def read_gps_data(self):
-        data = self.gpsd.next()
-        if data['class'] == 'TPV':
-            return {
-                'latitude': data['lat'],
-                'longitude': data['lon'],
-                'altitude': data['alt'],
-                'speed': data['speed'],
-                'timestamp': data['time'],
-                'satellites': data['satellites'],
-                'quality': data['fix'],
-                'pdop': data['pdop'],
-                'hdop': data['hdop'],
-                'vdop': data['vdop'],
-            }
-        else:
-            return None
+# Get gps position
+packet = gpsd.get_current()
 
-    def close(self):
-        self.gpsd.close()
-
-gps_interface = GPSInterface()
-while True:
-    # Get gps position
-    packet = gps_interface.gpsd.get_current()
-
-    print("Latitude: ", packet.lat)
-    print("Longitude: ", packet.lon)
-    print("Altitude: ", packet.alt)
-    print("Speed: ", packet.speed)
-    
-    # Wait for a bit before getting the next packet
-    time.sleep(1)
+# Print some data
+print("Latitude: ", packet.lat)
+print("Longitude: ", packet.lon)
+print("Altitude: ", packet.alt)
+print("Speed: ", packet.speed)
