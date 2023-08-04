2eee749c277d77fcd03b82d297489b544999575b
acredsfan <acredsfan@gmail.com>
2023-08-04 15:33:26 -0400

    Update test_camera.py

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,25 +1,9 @@
 import cv2
 
-# Define the GStreamer pipeline
-pipeline = (
-    "v4l2src device=/dev/video0 ! "
-    "videoconvert ! "
-    "appsink"
-)
+cap = cv2.VideoCapture(0) # 0 for the default camera
 
-# Open the camera using the defined pipeline
-cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+ret, frame = cap.read()
+if ret:
+    cv2.imwrite('test_image.jpg', frame)
 
-try:
-    ret, frame = cap.read()
-
-    if ret:
-        cv2.imwrite('test_image.jpg', frame)
-        print('Image captured successfully.')
-    else:
-        print('Failed to capture image')
-
-    cap.release()
-
-except KeyboardInterrupt:
-    print('Aborted by user (Ctrl + C).')
+cap.release()
