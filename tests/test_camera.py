5c91c03e038c0fa162ecbb63702594a31abdf74f
acredsfan <acredsfan@gmail.com>
2023-08-04 15:27:55 -0400

    Update test_camera.py

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -1,34 +1,17 @@
 import cv2
 
 try:
-    # Change this to the path of your camera
-    camera_path = '/dev/video0'
+    cap = cv2.VideoCapture('v4l2src device=/dev/video0 ! videoconvert ! ximagesink', cv2.CAP_GSTREAMER)
 
-    # Create a VideoCapture object
-    cap = cv2.VideoCapture(camera_path)
+    ret, frame = cap.read()
 
-    # Check if the camera opened successfully
-    if not cap.isOpened():
-        print(f"Error opening camera at {camera_path}")
+    if ret:
+        cv2.imwrite('test_image.jpg', frame)
+        print('Image captured successfully.')
     else:
-        print(f"Successfully opened camera at {camera_path}")
-
-        # Capture a single frame
-        ret, frame = cap.read()
-
-        # Check if the frame was captured successfully
-        if ret:
-            print("Frame captured successfully")
-            cv2.imshow('Frame', frame)
-            cv2.waitKey(0)
-        else:
-            print("Failed to capture frame")
-
-        # Release the VideoCapture object
-        cap.release()
-        cv2.destroyAllWindows()
+        print('Failed to capture image')
+        
+    cap.release()
 
 except KeyboardInterrupt:
     print('Aborted by user (Ctrl + C).')
-
-import cv2
