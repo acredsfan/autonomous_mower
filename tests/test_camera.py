a5ceea22bf746296a4a0ff41484b7ff41d108867
acredsfan <acredsfan@gmail.com>
2023-08-31 13:41:31 -0400

    Update test_camera.py

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -2,6 +2,7 @@ import cv2
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
+cap = None  # Initialize outside try block
 
 try:
     cap = cv2.VideoCapture(0)
@@ -10,10 +11,15 @@ try:
     else:
         ret, frame = cap.read()
         if ret:
-            cv2.imshow('Test Frame', frame)
+            #cv2.imshow('Test Frame', frame)
             cv2.waitKey(0)
         else:
             print("Error: Couldn't read a frame from the camera.")
+except KeyboardInterrupt:
+    print("Interrupted.")
+except Exception as e:
+    print(f"An error occurred: {e}")
 finally:
-    cap.release()
+    if cap:
+        cap.release()
     cv2.destroyAllWindows()
