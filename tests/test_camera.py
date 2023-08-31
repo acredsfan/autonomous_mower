211eb76cc35f63c21561b021f3d01521b8dcd4b2
acredsfan <acredsfan@gmail.com>
2023-08-31 13:25:09 -0400

    Update test_camera.py

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -3,10 +3,17 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-cap = cv2.VideoCapture(0) # 0 for the default camera
-
-ret, frame = cap.read()
-if ret:
-    cv2.imwrite('test_image.jpg', frame)
-
-cap.release()
+try:
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error: Couldn't open the camera.")
+    else:
+        ret, frame = cap.read()
+        if ret:
+            cv2.imshow('Test Frame', frame)
+            cv2.waitKey(0)
+        else:
+            print("Error: Couldn't read a frame from the camera.")
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
