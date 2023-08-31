e5ddbefeadd663577ae20f89c473658e82954b6d
acredsfan <acredsfan@gmail.com>
2023-08-31 15:26:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ lock = Lock()
 
 def check_mowing_conditions():
     try:
-        if sensor_interface.ideal_mowing_conditions() and sensor_interface.check_weather():
+        if sensor_interface.ideal_mowing_conditions():
             if not mower_blades_on:
                 BladeController.set_speed(90)
                 return True
