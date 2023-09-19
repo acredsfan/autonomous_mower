6dd481006a173da425849dd4e787ede61f79f8dd
acredsfan <acredsfan@gmail.com>
2023-09-19 17:21:18 -0400

    Update gps_interface.py

--- navigation_system/gps_interface.py
+++ navigation_system/gps_interface.py
@@ -24,7 +24,7 @@ class GPSInterface:
 
     def read_gps_data(self):
         packet = gpsd.get_current()
-        if packet.mode >= 3:  # 2D or 3D fix
+        if packet.mode >= 2:  # 2D or 3D fix
             return {
                 'latitude': packet.lat,
                 'longitude': packet.lon,
