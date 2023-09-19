44c2d1333e8a85c70a42697071f7c7caf7ca3ca7
acredsfan <acredsfan@gmail.com>
2023-09-19 15:25:09 -0400

    Update test_tof.py

--- tests/test_tof.py
+++ tests/test_tof.py
@@ -1,30 +1,69 @@
+"""
+Example of how to use the adafruit_vl53l0x library to change the assigned address of
+multiple VL53L0X sensors on the same I2C bus. This example only focuses on 2 VL53L0X
+sensors, but can be modified for more. BE AWARE: a multitude of sensors may require
+more current than the on-board 3V regulator can output (typical current consumption during
+active range readings is about 19 mA per sensor).
+"""
+import time
 import board
-import busio
-import adafruit_tca9548a
-import adafruit_vl53l0x
+from digitalio import DigitalInOut
+from adafruit_vl53l0x import VL53L0X
 
-# Initialize I2C bus and sensor.
-i2c = busio.I2C(board.SCL, board.SDA)
+# declare the singleton variable for the default I2C bus
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
-# Initialize TCA9548A multiplexer
-tca = adafruit_tca9548a.TCA9548A(i2c, address=0x70)
+# declare the digital output pins connected to the "SHDN" pin on each VL53L0X sensor
+xshut = [
+    DigitalInOut(board.D22),
+    DigitalInOut(board.D29),
+    # add more VL53L0X sensors by defining their SHDN pins here
+]
 
-# Initialize VL53L0X on channel 6 and 7 of TCA9548A
-vl53_left = adafruit_vl53l0x.VL53L0X(tca[6])
-vl53_right = adafruit_vl53l0x.VL53L0X(tca[7])
+for power_pin in xshut:
+    # make sure these pins are a digital output, not a digital input
+    power_pin.switch_to_output(value=False)
+    # These pins are active when Low, meaning:
+    #   if the output signal is LOW, then the VL53L0X sensor is off.
+    #   if the output signal is HIGH, then the VL53L0X sensor is on.
+# all VL53L0X sensors are now off
 
-# Set shutdown pins
-vl53_left._shutdown_pin = 22
-vl53_right._shutdown_pin = 23
+# initialize a list to be used for the array of VL53L0X sensors
+vl53 = []
 
-# Function to read distance
-def read_distance(sensor, name):
-    try:
-        print(f"{name} Distance: {sensor.range}mm")
-    except Exception as e:
-        print(f"Error reading {name}: {e}")
+# now change the addresses of the VL53L0X sensors
+for i, power_pin in enumerate(xshut):
+    # turn on the VL53L0X to allow hardware check
+    power_pin.value = True
+    # instantiate the VL53L0X sensor on the I2C bus & insert it into the "vl53" list
+    vl53.insert(i, VL53L0X(i2c))  # also performs VL53L0X hardware check
+    # no need to change the address of the last VL53L0X sensor
+    if i < len(xshut) - 1:
+        # default address is 0x29. Change that to something else
+        vl53[i].set_address(i + 0x30)  # address assigned should NOT be already in use
+# there is a helpful list of pre-designated I2C addresses for various I2C devices at
+# https://learn.adafruit.com/i2c-addresses/the-list
+# According to this list 0x30-0x34 are available, although the list may be incomplete.
+# In the python REPR, you can scan for all I2C devices that are attached and detirmine
+# their addresses using:
+#   >>> import board
+#   >>> i2c = board.I2C()
+#   >>> if i2c.try_lock():
+#   >>>     [hex(x) for x in i2c.scan()]
+#   >>>     i2c.unlock()
 
-# Main loop will read the range and print it every second.
-while True:
-    read_distance(vl53_left, "Left Sensor")
-    read_distance(vl53_right, "Right Sensor")
+
+def detect_range(count=5):
+    """take count=5 samples"""
+    while count:
+        for index, sensor in enumerate(vl53):
+            print("Sensor {} Range: {}mm".format(index + 1, sensor.range))
+        time.sleep(1.0)
+        count -= 1
+
+
+print(
+    "Multiple VL53L0X sensors' addresses are assigned properly\n"
+    "execute detect_range() to read each sensors range readings"
+)
