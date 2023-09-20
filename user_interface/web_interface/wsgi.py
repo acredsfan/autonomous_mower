03347f8f153650a732adb16e6397f6eb69d937de
acredsfan <acredsfan@gmail.com>
2023-09-20 16:08:05 -0400

    Update wsgi.py

--- user_interface/web_interface/wsgi.py
+++ user_interface/web_interface/wsgi.py
@@ -1,4 +1,4 @@
-from user_interface.web_interface.app import start_web_interface
+from user_interface.web_interface.app import start_web_interface, app
 
 if __name__ == "__main__":
     start_web_interface()
