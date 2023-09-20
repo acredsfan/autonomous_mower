879d79c16f6f398a5bd36b25c13ee3179a6a387d
acredsfan <acredsfan@gmail.com>
2023-09-20 17:13:25 -0400

    Update direction_controller.py

--- control_system/direction_controller.py
+++ control_system/direction_controller.py
@@ -1,36 +1,28 @@
-# direction_controller.py
-# Python code to control the direction of an autonomous robot based on sensor input
-# Running on Raspberry Pi 4B 2GB RAM with Raspbian Bullseye OS
-
 import time
-from hardware_interface import MotorController
 import logging
+from hardware_interface import MotorController
 from hardware_interface.sensor_interface import SensorInterface
+import logging
 
 # Initialize logging
 logging.basicConfig(filename='main.log', level=logging.DEBUG)
 
-# Initialize the hardware interface
-sensor_interface = SensorInterface()
-motor_controller = MotorController()
-
-# Set the distance threshold for obstacle avoidance (in millimeters)
-DISTANCE_THRESHOLD = 300
-
-# Set time intervals for various actions
-CHECK_INTERVAL = 0.5
-TURN_INTERVAL = 1
-
 class DirectionController:
-# Check if an obstacle is detected on the left or right side of the robot
-    def obstacle_detected():
-        left_distance = sensor_interface.read_vl53l0x_left()
-        right_distance = sensor_interface.read_vl53l0x_right()
+    def __init__(self):
+        self.DISTANCE_THRESHOLD = 300
+        self.CHECK_INTERVAL = 0.5
+        self.TURN_INTERVAL = 1
+        self.sensor_interface = SensorInterface()
+        self.motor_controller = MotorController()
+        
+        logging.basicConfig(filename='main.log', level=logging.DEBUG)
 
-        return left_distance < DISTANCE_THRESHOLD, right_distance < DISTANCE_THRESHOLD
+    def obstacle_detected(self):
+        left_distance = self.sensor_interface.read_vl53l0x_left()
+        right_distance = self.sensor_interface.read_vl53l0x_right()
+        return left_distance < self.DISTANCE_THRESHOLD, right_distance < self.DISTANCE_THRESHOLD
 
-    # Determine the direction to turn based on obstacle detection
-    def choose_turn_direction(left_obstacle, right_obstacle):
+    def choose_turn_direction(self, left_obstacle, right_obstacle):
         if left_obstacle and right_obstacle:
             return "backward"
         elif left_obstacle:
@@ -40,28 +32,30 @@ class DirectionController:
         else:
             return "forward"
 
-    # Main function to control the robot direction based on sensor input
-    def control_direction():
-        sensor_interface.init_sensors()
-        motor_controller.init_motor_controller()
+    def control_direction(self):
+        self.sensor_interface.init_sensors()
+        self.motor_controller.init_motor_controller()
 
         while True:
-            left_obstacle, right_obstacle = obstacle_detected()
-
+            left_obstacle, right_obstacle = self.obstacle_detected()
             if left_obstacle or right_obstacle:
-                direction = choose_turn_direction(left_obstacle, right_obstacle)
-                motor_controller.set_motor_direction(direction)
-                motor_controller.set_motor_speed(50, 50)
-                time.sleep(TURN_INTERVAL)
+                direction = self.choose_turn_direction(left_obstacle, right_obstacle)
+                self.motor_controller.set_motor_direction(direction)
+                self.motor_controller.set_motor_speed(50, 50)
+                time.sleep(self.TURN_INTERVAL)
             else:
-                motor_controller.set_motor_direction("forward")
-                motor_controller.set_motor_speed(50, 50)
-                time.sleep(CHECK_INTERVAL)
+                self.motor_controller.set_motor_direction("forward")
+                self.motor_controller.set_motor_speed(50, 50)
+                time.sleep(self.CHECK_INTERVAL)
 
-    if __name__ == "__main__":
-        try:
-            control_direction()
-        except KeyboardInterrupt:
-            motor_controller.stop_motors()
-            motor_controller.cleanup()
-            sensor_interface.cleanup()
+def main():
+    direction_controller = DirectionController()
+    try:
+        direction_controller.control_direction()
+    except KeyboardInterrupt:
+        direction_controller.motor_controller.stop_motors()
+        direction_controller.motor_controller.cleanup()
+        direction_controller.sensor_interface.cleanup()
+
+if __name__ == "__main__":
+    main()
