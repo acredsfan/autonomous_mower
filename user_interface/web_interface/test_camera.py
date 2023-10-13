e9f0db2d5964fcbec8564aecbc987aa0cbaafb71
acredsfan <acredsfan@gmail.com>
2023-10-13 11:15:09 -0400

    Update test_camera.py

--- user_interface/web_interface/test_camera.py
+++ user_interface/web_interface/test_camera.py
@@ -5,6 +5,8 @@ for i in range(0, 10):
     cap = cv2.VideoCapture(i)
     if cap.read()[0]:
         print(f'Camera detected at index {i}')
+        ret, frame = cap.read()
+        cv2.imwrite(f'camera_test_{i}.jpg', frame)
         cap.release()
     else:
         print(f'No camera detected at index {i}')
