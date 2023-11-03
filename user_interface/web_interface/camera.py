a7e1e63fbd321d7cf785e116809c46c80aa23ab8
acredsfan <acredsfan@gmail.com>
2023-11-03 15:47:01 -0400

    Update camera.py

--- user_interface/web_interface/camera.py
+++ user_interface/web_interface/camera.py
@@ -20,16 +20,24 @@ class SingletonCamera:
             self.cap = None
 
     def get_frame(self):
-        if not self.cap:
+        if self.cap is None:
+            logging.error("Camera capture is not initialized.")
             return None
         try:
             ret, frame = self.cap.read()
             if not ret:
-                logging.error("Failed to grab frame.")
+                logging.error("cap.read() returned False. Unable to grab frame.")
+                # Log additional debug information if needed
+                logging.debug(f"cap.read() return value: {ret}")
+                logging.debug(f"cap.isOpened(): {self.cap.isOpened()}")
+                # If you want to log the entire stack trace, uncomment the following line:
+                logging.error("Stack Trace:", exc_info=True)
                 return None
             return frame
         except Exception as e:
-            logging.error(f"Exception: {e}")
+            logging.error(f"Exception occurred while reading frame: {e}")
+            # Log the stack trace for the exception
+            logging.error(traceback.format_exc())
             return None
 
     def __del__(self):
