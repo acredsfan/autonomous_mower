017566d3def132c48dba489f6363743ead3eb85f
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-07-29 11:01:25 -0400

    Update simple_bme280_test.py

--- simple_bme280_test.py
+++ simple_bme280_test.py
@@ -3,7 +3,7 @@ import busio
 from adafruit_bme280 import basic as adafruit_bme280
 
 # Initialize I2C bus
-i2c = busio.I2C(board.GP0, board.GP1)
+i2c = busio.I2C(board.SCL, board.SDA)
 
 # Initialize BME280 sensor
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x76)
