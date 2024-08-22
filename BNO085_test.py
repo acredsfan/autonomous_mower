6ad4f3d032d112d68c5bea753a92b4db32cf2d8b
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-08-22 13:24:35 -0400

    Update BNO085_test.py

--- BNO085_test.py
+++ BNO085_test.py
@@ -10,7 +10,7 @@ from adafruit_bno08x import (
 from adafruit_bno08x.i2c import BNO08X_I2C
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
-bno = BNO08X_I2C(i2c)
+bno = BNO08X_I2C(i2c, address=0x4B)  # Specify the address here
 
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
 bno.enable_feature(BNO_REPORT_GYROSCOPE)
