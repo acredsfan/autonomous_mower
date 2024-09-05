d4471b8346ae3d15bd055689aa0548370977a445
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-09-05 14:18:47 -0400

    Update bno085_sensor.py

--- hardware_interface/bno085_sensor.py
+++ hardware_interface/bno085_sensor.py
@@ -1,4 +1,6 @@
 import logging
+import board
+import busio
 from adafruit_bno08x import (
     BNO_REPORT_ACCELEROMETER,
     BNO_REPORT_GYROSCOPE,
@@ -6,6 +8,8 @@ from adafruit_bno08x import (
     BNO_REPORT_ROTATION_VECTOR,
 )
 from adafruit_bno08x.i2c import BNO08X_I2C
+
+i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
 
 class BNO085Sensor:
 
