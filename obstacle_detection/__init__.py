7cda82a73b3a054aee41ec81357de9bb2dac6ff9
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-09-06 20:05:31 -0400

    Update __init__.py

--- obstacle_detection/__init__.py
+++ obstacle_detection/__init__.py
@@ -4,10 +4,8 @@
 #IMPORTS
 from .avoidance_algorithm import AvoidanceAlgorithm
 from .camera_processing import CameraProcessor
-from .tof_processing import ObstacleAvoidance
 
 __all__ = [
     'AvoidanceAlgorithm',
     'CameraProcessor',
-    'ObstacleAvoidance',
 ]
