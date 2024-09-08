e75e1aa5d78092b87aea531efeb5f1bf10a66405
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-09-08 13:48:41 -0400

    Update test_bno085.py

--- tests/test_bno085.py
+++ tests/test_bno085.py
@@ -25,22 +25,22 @@ while True:
         print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
         print("")
 
-        print("Gyro:")
-        gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
-        print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
-        print("")
+        # print("Gyro:")
+        # gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
+        # print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
+        # print("")
 
-        print("Magnetometer:")
-        mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
-        print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
-        print("")
+        # print("Magnetometer:")
+        # mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
+        # print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
+        # print("")
 
-        print("Rotation Vector Quaternion:")
-        quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
-        print(
-            "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
-        )
-        print("")
+        # print("Rotation Vector Quaternion:")
+        # quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
+        # print(
+        #     "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
+        # )
+        # print("")
 
     except Exception as e:
         print(f"Error: {e}")
