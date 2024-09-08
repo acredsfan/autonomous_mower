4a8d93fa0c976fd101fb6b52f9f2f912ebb8cd3e
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-09-08 13:48:12 -0400

    Update test_bno085.py

--- tests/test_bno085.py
+++ tests/test_bno085.py
@@ -9,34 +9,40 @@ from adafruit_bno08x import (
 )
 from adafruit_bno08x.i2c import BNO08X_I2C
 
-i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
+i2c = busio.I2C(board.SCL, board.SDA)
 bno = BNO08X_I2C(i2c, address=0x4B)  # Specify the address here
 
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
-bno.enable_feature(BNO_REPORT_GYROSCOPE)
-bno.enable_feature(BNO_REPORT_MAGNETOMETER)
-bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+#bno.enable_feature(BNO_REPORT_GYROSCOPE)
+#bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+#bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
 
 while True:
-    time.sleep(1)
-    print("Acceleration:")
-    accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-    print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
-    print("")
+    try:
+        time.sleep(1)
+        print("Acceleration:")
+        accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
+        print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
+        print("")
 
-    print("Gyro:")
-    gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
-    print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
-    print("")
+        print("Gyro:")
+        gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
+        print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
+        print("")
 
-    print("Magnetometer:")
-    mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
-    print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
-    print("")
+        print("Magnetometer:")
+        mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
+        print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
+        print("")
 
-    print("Rotation Vector Quaternion:")
-    quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
-    print(
-        "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
-    )
-    print("")
+        print("Rotation Vector Quaternion:")
+        quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
+        print(
+            "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
+        )
+        print("")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        time.sleep(1)
+        continue
