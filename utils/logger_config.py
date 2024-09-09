8c783865be00f05380ffaaacff046b3479196e4f
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-09-08 21:43:23 -0400

    Create logger_config.py

---
+++ utils/logger_config.py
@@ -0,0 +1,11 @@
+import logging
+
+def configure_logging(log_file='main.log'):
+    logging.basicConfig(
+        filename=log_file,
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s'
+    )
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    logging.getLogger().addHandler(console_handler)
