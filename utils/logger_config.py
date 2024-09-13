2bc7989e1523da41c2e4acf78c11052103a07c5a
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-09-13 14:24:12 -0400

    Update logger_config.py

--- utils/logger_config.py
+++ utils/logger_config.py
@@ -3,11 +3,15 @@ import logging
 
 class LoggerConfig:
     @staticmethod
-    def configure_logging(log_file='main.log'):
+    def configure_logging(log_file="main.log"):
         logging.basicConfig(
             filename=log_file,
             level=logging.DEBUG,
-            format=('%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s'))
+            format=(
+                "%(asctime)s - %(name)s - %(levelname)s - "
+                "%(filename)s:%(lineno)d - %(message)s"
+            ),
+        )
         console_handler = logging.StreamHandler()
         console_handler.setLevel(logging.INFO)
         logging.getLogger().addHandler(console_handler)
