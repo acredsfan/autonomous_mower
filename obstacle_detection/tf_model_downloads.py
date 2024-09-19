4463c7a7ebeddc7f880e0a481d50e6da2c0e6959
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-09-19 14:23:06 -0400

    Update tf_model_downloads.py

--- obstacle_detection/tf_model_downloads.py
+++ obstacle_detection/tf_model_downloads.py
@@ -1,7 +1,6 @@
 import kagglehub
 
-# Download mobilenet-v2 for object and surface detection
-path = kagglehub.model_download(
-    "google/mobilenet-v2/tensorFlow2/100-224-classification")
-
-print("Path to model files:", path)
+# Download mobilenet-v2 for object and surface detection and save to object_detection folder
+kagglehub.download("kaggle/input/obstacle-detection-tf-models/mobilenet_v2_1.0_224_quant.tflite", "obstacle_detection")
+# Print path to downloaded model
+print("Downloaded model to obstacle_detection folder.")
