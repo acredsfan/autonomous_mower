09795fa6f4c69512cc9bd8fec6d9c1fe790585cd
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-09-20 17:01:18 -0400

    Update test_sensor_readings.py

--- tests/test_sensor_readings.py
+++ tests/test_sensor_readings.py
@@ -1,6 +1,9 @@
 # Test to ensure that the sensor readings are being read and reported correctly
 # import SensorInterface from hardware_interface folder
 import time
+import os
+import sys
+
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
