9ec7e7a78b218283a14a0cadfd086b6f416119f1
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-09-25 12:15:56 -0400

    Update main.js

--- user_interface/web_interface/static/js/main.js
+++ user_interface/web_interface/static/js/main.js
@@ -139,7 +139,10 @@ async function initMap() {
         drawingControl: true,
         drawingControlOptions: {
             position: google.maps.ControlPosition.TOP_CENTER,
-            drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+            drawingModes: [
+                google.maps.drawing.OverlayType.POLYGON,
+                google.maps.drawing.OverlayType.MARKER
+            ]
         }
     });
 
