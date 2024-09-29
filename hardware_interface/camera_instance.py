0db340de64d39bb9220386c5e8948b876944dce2
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-09-29 13:19:40 -0400

    updating video stream handling

--- hardware_interface/camera_instance.py
+++ hardware_interface/camera_instance.py
@@ -1,10 +1,22 @@
 from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FfmpegOutput
 
 # Initialize Picamera2 instance
 camera = Picamera2()
-camera_config = camera.create_video_configuration(main={"size": (640, 480)})
+camera_config = camera.create_video_configuration(main={"size": (1280, 720)})
 camera.configure(camera_config)
+encoder = H264Encoder(1000000)
+# Set output of UDP stream to the Device's IP, Port 8000
+output = FfmpegOutput("-f rtp udp://{}:8000")
 camera.start()
 
+def start_server_thread():
+    """Start the server thread for streaming."""
+    camera.start_recording(encoder, output)
+
+
 def get_camera_instance():
+    """Get the camera instance and start server thread."""
+    start_server_thread()
     return camera
