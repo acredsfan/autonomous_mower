4e3f8c4db8b62e8dd79652fb40e2968cc28661ef
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-10-07 17:17:28 -0400

    Update robohat_comm_test.py

--- tests/robohat_comm_test.py
+++ tests/robohat_comm_test.py
@@ -5,6 +5,17 @@ ser = serial.Serial('/dev/serial0', 115200, timeout=1)
 time.sleep(2)  # Wait for the serial connection to initialize
 
 test_command = b'1500, 1500\r'
+test_command_2 = b'1600, 1600\r'
+test_command_3 = b'1400, 1400\r'
+ser.write(test_command)
+print(f"Sent: {test_command}")
+time.sleep(2)
+ser.write(test_command_2)
+print(f"Sent: {test_command_2}")
+time.sleep(2)
+ser.write(test_command_3)
+print(f"Sent: {test_command_3}")
+time.sleep(2)
 ser.write(test_command)
 print(f"Sent: {test_command}")
 
