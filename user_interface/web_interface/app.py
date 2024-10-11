dc05bc7166dab9bbf25966be33db2bf221aef07a
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-10-11 15:43:36 -0400

    Update app.py

--- user_interface/web_interface/app.py
+++ user_interface/web_interface/app.py
@@ -579,7 +579,7 @@ def on_connect(client, userdata, flags, rc, properties=None):
             logging.info("Mowing area thread started.")
 
 
-def on_publish(client, userdata, mid, reason_code, properties=None):
+def on_publish(client, userdata, mid):
     logging.info(f"Published message ID: {mid}")
 
 
