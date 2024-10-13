f65065ee00e3b93c5841e1afe090293a57d04c12
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-10-13 17:42:42 -0400

    Update blade_test.py

--- blade_test.py
+++ blade_test.py
@@ -1,5 +1,28 @@
 from hardware_interface.blade_controller import BladeController
 import time
+from hardware_interface.gpio_manager import GPIOManager
+import time
+
+# Test GPIO pins used for blade motor
+shutdown_pins = [24, 25]
+shutdown_lines, _ = GPIOManager.init_gpio(shutdown_pins, [])
+
+print("Turning on pin 24")
+shutdown_lines[0].set_value(1)  # Set pin 24 to HIGH
+time.sleep(5)
+
+print("Turning off pin 24")
+shutdown_lines[0].set_value(0)  # Set pin 24 to LOW
+time.sleep(2)
+
+print("Turning on pin 25")
+shutdown_lines[1].set_value(1)  # Set pin 25 to HIGH
+time.sleep(5)
+
+print("Turning off pin 25")
+shutdown_lines[1].set_value(0)  # Set pin 25 to LOW
+
+print("GPIO test complete")
 
 # Initialize blade controller instance
 blade_controller = BladeController()
