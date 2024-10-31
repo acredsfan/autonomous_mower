493ea9920a8148a4724ee94f4485e2918422d9e2
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-10-31 12:11:50 -0400

    Update wsgi.py

--- src/mower/ui/web_ui/wsgi.py
+++ src/mower/ui/web_ui/wsgi.py
@@ -6,9 +6,15 @@ from mower.utilities.logger_config import LoggerConfigInfo as LoggerConfig
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+def start_web_interface():
+    # Check if the web interface is already running
+    if WebInterface.is_running():
+        logging.warning("Web interface is already running.")
+        return
+    WebInterface.start()
 
 # Initialize logger
 logging = LoggerConfig.get_logger(__name__)
 
 if __name__ == "__main__":
-    WebInterface.start()
+    start_web_interface()
