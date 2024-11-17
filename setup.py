1483db083400bed00199d0d422a2b9e38a2168c2
acredsfan <65324894+acredsfan@users.noreply.github.com>
2024-11-17 12:58:34 -0500

    Update entry point in setup.py
    Changed the entry point from mower.robot to mower.mower in setup.py to correctly map to the new module structure. This ensures the autonomous_mower script points to the appropriate main function.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'autonomous_mower=mower.robot:main',
+            'autonomous_mower=mower.mower:main',
         ],
     },
     author='Aaron Link',
