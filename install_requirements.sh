8df6642a02b5750d8c84fa39a67a1b0a1894ad8f
acredsfan <65324894+acredsfan@users.noreply.github.com>
2025-05-11 09:35:58 -0400

    Fix formatting of color codes in install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -4,10 +4,10 @@
 set -e
 
 # Color codes for output
-RED=\'\\033[0;31m\'
-GREEN=\'\\033[0;32m\'
-YELLOW=\'\\033[1;33m\'
-NC=\'\\033[0m\' # No Color
+RED='\\033[0;31m'
+GREEN='\\033[0;32m'
+YELLOW='\\033[1;33m'
+NC='\\033[0m' # No Color
 
 # Global variable to collect messages for the end
 POST_INSTALL_MESSAGES=""
